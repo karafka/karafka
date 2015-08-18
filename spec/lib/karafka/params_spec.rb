17de0a83ef2785d15708155c0257b310f0715119
pavlo_vavruk <pavlo.vavruk@gmail.com>
2015-08-18 14:39:28 +0200

    Change params spec

--- spec/lib/karafka/params_spec.rb
+++ spec/lib/karafka/params_spec.rb
@@ -2,24 +2,27 @@ require 'spec_helper'
 
 RSpec.describe Karafka::Params do
   describe '#parse' do
-    let(:options) { { a: '1' } }
+    let(:opt) { { a: '1' } }
+    subject { described_class.new(options) }
+
     context 'params is json' do
-      before do
-        @parsed_params = described_class.new(options.to_json).parse
-      end
+      let(:options) { opt.to_json }
+      let(:parsed_params) { subject.parse }
 
       it 'deserializes as JSON' do
-        expect(@parsed_params[:a]).to eq '1'
+        expect(parsed_params[:a]).to eq '1'
       end
 
       it 'returns hashWithIndifferentAccess' do
-        expect(@parsed_params[:a]).to eq(@parsed_params['a'])
+        expect(parsed_params[:a]).to eq(parsed_params['a'])
       end
     end
 
     context 'params is string' do
+      let(:options) { opt.to_s }
+
       it 'deserializes as String' do
-        parsed_params = described_class.new(options.to_s).parse
+        parsed_params = subject.parse
         expect(parsed_params).to eq({ a: '1' }.to_s)
       end
     end
