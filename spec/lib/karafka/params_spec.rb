5c6aceaadfc5aa3f74ff0a2edbdf53eb0d57da4f
pavlo_vavruk <pavlo.vavruk@gmail.com>
2015-08-18 14:42:43 +0200

    Change params spec

--- spec/lib/karafka/params_spec.rb
+++ spec/lib/karafka/params_spec.rb
@@ -3,11 +3,11 @@ require 'spec_helper'
 RSpec.describe Karafka::Params do
   describe '#parse' do
     let(:opt) { { a: '1' } }
+    let(:parsed_params) { subject.parse }
     subject { described_class.new(options) }
 
     context 'params is json' do
       let(:options) { opt.to_json }
-      let(:parsed_params) { subject.parse }
 
       it 'deserializes as JSON' do
         expect(parsed_params[:a]).to eq '1'
@@ -22,8 +22,7 @@ RSpec.describe Karafka::Params do
       let(:options) { opt.to_s }
 
       it 'deserializes as String' do
-        parsed_params = subject.parse
-        expect(parsed_params).to eq({ a: '1' }.to_s)
+        expect(parsed_params).to eq(options)
       end
     end
   end
