8246afadada5c2c79f086d96aae16e80a6ac2cae
Maciej Mensfeld <maciej@mensfeld.pl>
2015-08-26 18:33:42 +0200

    signals

--- lib/karafka/monitor.rb
+++ lib/karafka/monitor.rb
@@ -1,57 +1,60 @@
 module Karafka
-  # Class used to catch signals from ruby Signal class
+  # Class used to catch signals from ruby Signal class in order to manage Karafka shutdown
+  # @note There might be only one monitor - this class is a singleton
   class Monitor
-    attr_reader :terminated
+    include Singleton
 
-    # Default list of signals which we want to catch
-    DEFAULT_SIGNALS = %i(
-      SIGINT
+    # Signal types that we handle
+    HANDLED_SIGNALS = %i(
+      SIGINT SIGQUIT
     )
 
-    # Setting terminated flag as false
-    # Setting instance variable signals from param or DEFAULT_SIGNALS
-    # @param signals [Array<Symbol>] list of signals
-    #   matching with Signal.list
-    def initialize(signals = DEFAULT_SIGNALS)
-      @terminated = false
-      @signals = signals
+    HANDLED_SIGNALS.each do |signal|
+      # Assigns a callback that will happen when certain signal will be send
+      # to Karafka server instance
+      # @note It does not define the callback itself -it needs to be passed in a block
+      # @example Define an action that should be taken on_sigint
+      #   monitor.on_sigint do
+      #     Karafka.logger.info('Log something here')
+      #     exit
+      #   end
+      define_method :"on_#{signal.to_s.downcase}" do |&block|
+        @callbacks[signal] << block
+      end
     end
 
-    # Method catch signals defined in @signals variable
-    # @param [Block] block of code that we want to execute
-    # and reset signals to default values [SIG_DFL]
-    def catch_signals(&block)
-      trap_signals
+    # Creates an instance of monitor and creates empty hash for callbacks
+    def initialize
+      @callbacks = {}
+      HANDLED_SIGNALS.each { |signal| @callbacks[signal] = [] }
+    end
+
+    # Method catches all HANDLED_SIGNALS and performs appropriate callbacks (if defined)
+    # @note If there are no callbacks, this method will just ignore a given signal that was sent
+    # @param [Block] block of code that we want to execute and supervise
+    def supervise(&block)
+      HANDLED_SIGNALS.each { |signal| trap_signal(signal) }
       block.call
-      reset_signals
     end
 
     private
 
-    # Trapping and ignoring all signals defined in @signals variable
-    # Setting @terminated flag as true
-    # Logging error
-    def trap_signals
-      @signals.each do |s|
-        trap(s) do
-          @terminated = true
-          log_error(s)
-        end
+    # Traps a single signal and performs callbacks (if any) or just ignores this signal
+    # @param [Symbol] signal type that we want to catch
+    def trap_signal(signal)
+      trap(signal) do
+        log_signal(signal)
+        (@callbacks[signal] || []).each(&:call)
       end
     end
 
     # Logging into Karafka.logger error with signal code
-    # @param [Symbol] signal name
-    def log_error(signal)
+    # @param [Symbol] signal type that we received
+    # @note We cannot perform logging from trap context, that's why
+    #   we have to spin up a new thread to do this
+    def log_signal(signal)
       Thread.new do
-        Karafka.logger.error("Terminating with signal #{signal}")
-      end
-    end
-
-    # Reset signals from @signal variable into default value [SIG_DFL]
-    def reset_signals
-      @signals.each do |s|
-        trap(s, :SIG_DFL)
+        Karafka.logger.info("Received system signal #{signal}")
       end
     end
   end
--- spec/lib/karafka/monitor_spec.rb
+++ spec/lib/karafka/monitor_spec.rb
@@ -1,93 +1,73 @@
 require 'spec_helper'
 
 RSpec.describe Karafka::Monitor do
-  subject { described_class.new }
+  subject { described_class.instance }
 
-  describe '#new' do
-    context 'with default signals' do
-      it 'should set DEFAULT_SIGNALS to @signals' do
-        expect(subject.instance_variable_get('@signals'))
-          .to eq described_class::DEFAULT_SIGNALS
+  described_class::HANDLED_SIGNALS.each do |signal|
+    let(:callback) { -> {} }
+
+    describe "on_#{signal.to_s.downcase}" do
+      it 'should assign given callback to appropriate signal key' do
+        subject.send(:"on_#{signal.to_s.downcase}", &callback)
+        expect(subject.instance_variable_get(:@callbacks)[signal]).to include callback
       end
-    end
-
-    context 'with defined signals' do
-      let(:signals) { [rand.to_s, rand.to_s] }
-      it 'should set passed signals to @signals' do
-        expect(described_class.new(signals).instance_variable_get('@signals'))
-          .to eq signals
-      end
-    end
-
-    it 'should set @terminated false' do
-      expect(subject.instance_variable_get('@terminated')).to eq false
     end
   end
 
-  describe '#catch_signals' do
+  describe '#supervise' do
     let(:block) { -> {} }
-    it 'should trap signals, yield and reset signals' do
-      expect(subject)
-        .to receive(:trap_signals)
+
+    it 'should trap signals and yield' do
+      described_class::HANDLED_SIGNALS.each do |signal|
+        expect(subject)
+          .to receive(:trap_signal)
+          .with(signal)
+      end
 
       expect(block)
         .to receive(:call)
 
-      expect(subject)
-        .to receive(:reset_signals)
-
-      subject.send(:catch_signals, &block)
+      subject.send(:supervise, &block)
     end
   end
 
-  describe '#trap_signals' do
-    let(:signals) { [rand.to_s, rand.to_s] }
-    before do
-      subject.instance_variable_set('@signals', signals)
-      signals.each do |s|
-        expect(subject)
-          .to receive(:trap)
-          .and_yield
-
-        expect(subject)
-          .to receive(:log_error)
-          .with(s)
-      end
-    end
-    it 'should trap signals, log error and set terminated false' do
-      subject.send(:trap_signals)
-      expect(subject.instance_variable_get('@terminated')).to eq true
-    end
-  end
-
-  describe '#log_error' do
+  describe '#trap_signal' do
     let(:signal) { rand.to_s }
-    it 'should log error with signal code into Karafka logger' do
+    let(:callback) { double }
+
+    before do
+      subject.instance_variable_set(:'@callbacks', signal => [callback])
+
+      expect(subject)
+        .to receive(:trap)
+        .with(signal)
+        .and_yield
+    end
+
+    it 'should trap signals, log it and run callbacks if defined' do
+      expect(subject)
+        .to receive(:log_signal)
+        .with(signal)
+
+      expect(callback)
+        .to receive(:call)
+
+      subject.send(:trap_signal, signal)
+    end
+  end
+
+  describe '#log_signal' do
+    let(:signal) { rand.to_s }
+    it 'should log info with signal code into Karafka logger' do
       expect(Thread)
         .to receive(:new)
         .and_yield
 
       expect(Karafka.logger)
-        .to receive(:error)
-        .with("Terminating with signal #{signal}")
+        .to receive(:info)
+        .with("Received system signal #{signal}")
 
-      subject.send(:log_error, signal)
-    end
-  end
-
-  describe '#reset_signals' do
-    let(:signals) { [rand.to_s, rand.to_s] }
-    before do
-      subject.instance_variable_set('@signals', signals)
-    end
-
-    it 'should reset signals to default values' do
-      signals.each do |s|
-        expect(subject)
-          .to receive(:trap)
-          .with(s, :SIG_DFL)
-      end
-      subject.send(:reset_signals)
+      subject.send(:log_signal, signal)
     end
   end
 end
