1f0fc38207126a3aaa0ccd0569b297198e741e3b
Maciej Mensfeld <maciej@mensfeld.pl>
2017-08-01 21:59:30 +0200

    missing spec stub files and naming fix

---
+++ spec/lib/karafka/routing/consumer_group_spec.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+RSpec.describe Karafka::Routing::ConsumerGroup do
+  pending
+end
---
+++ spec/lib/karafka/schemas/consumer_group_spec.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+RSpec.describe Karafka::Schemas::ConsumerGroup do
+  let(:schema) { described_class }
+
+  pending
+end
