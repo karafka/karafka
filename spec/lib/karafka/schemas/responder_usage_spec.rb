96805af131a848f293075b5ab2587b119e6526c9
Maciej Mensfeld <maciej@mensfeld.pl>
2017-09-17 19:03:45 +0200

    small spec remarks

--- spec/lib/karafka/schemas/responder_usage_spec.rb
+++ spec/lib/karafka/schemas/responder_usage_spec.rb
@@ -27,7 +27,7 @@ RSpec.describe Karafka::Schemas::ResponderUsage do
     before { responder_usage[:used_topics] = [rand.to_s] }
 
     it 'expect not to allow that' do
-      expect(schema.call(responder_usage).success?).to be_falsey
+      expect(schema.call(responder_usage)).not_to be_success
     end
   end
 
@@ -48,19 +48,19 @@ RSpec.describe Karafka::Schemas::ResponderUsage do
       ).to_h.merge!(usage_count: usage_count)
     end
 
-    it { expect(subschema.call(topic_data).success?).to be_truthy }
+    it { expect(subschema.call(topic_data)).to be_success }
 
     context 'name validator' do
       context 'name is nil' do
         let(:name) { nil }
 
-        it { expect(subschema.call(topic_data).success?).to be_falsey }
+        it { expect(subschema.call(topic_data)).not_to be_success}
       end
 
       context 'name is an invalid string' do
         let(:name) { '%^&*(' }
 
-        it { expect(subschema.call(topic_data).success?).to be_falsey }
+        it { expect(subschema.call(topic_data)).not_to be_success }
       end
     end
 
@@ -68,13 +68,13 @@ RSpec.describe Karafka::Schemas::ResponderUsage do
       context 'required is nil' do
         let(:required) { nil }
 
-        it { expect(subschema.call(topic_data).success?).to be_falsey }
+        it { expect(subschema.call(topic_data)).not_to be_success }
       end
 
       context 'required is not a bool' do
         let(:required) { 2 }
 
-        it { expect(subschema.call(topic_data).success?).to be_falsey }
+        it { expect(subschema.call(topic_data)).not_to be_success }
       end
     end
 
@@ -82,7 +82,7 @@ RSpec.describe Karafka::Schemas::ResponderUsage do
       context 'multiple_usage is not a bool' do
         let(:multiple_usage) { 2 }
 
-        it { expect(subschema.call(topic_data).success?).to be_falsey }
+        it { expect(subschema.call(topic_data)).not_to be_success }
       end
     end
 
@@ -90,14 +90,14 @@ RSpec.describe Karafka::Schemas::ResponderUsage do
       let(:required) { true }
       let(:usage_count) { 0 }
 
-      it { expect(subschema.call(topic_data).success?).to be_falsey }
+      it { expect(subschema.call(topic_data)).not_to be_success }
     end
 
     context 'when we did use required topic' do
       let(:required) { true }
       let(:usage_count) { 1 }
 
-      it { expect(subschema.call(topic_data).success?).to be_truthy }
+      it { expect(subschema.call(topic_data)).to be_success }
     end
 
     context 'when we didnt use required topic with multiple_usage' do
@@ -105,7 +105,7 @@ RSpec.describe Karafka::Schemas::ResponderUsage do
       let(:usage_count) { 0 }
       let(:multiple_usage) { true }
 
-      it { expect(subschema.call(topic_data).success?).to be_falsey }
+      it { expect(subschema.call(topic_data)).not_to be_success }
     end
 
     context 'when we did use required topic with multiple_usage once' do
@@ -113,7 +113,7 @@ RSpec.describe Karafka::Schemas::ResponderUsage do
       let(:usage_count) { 1 }
       let(:multiple_usage) { true }
 
-      it { expect(subschema.call(topic_data).success?).to be_truthy }
+      it { expect(subschema.call(topic_data)).to be_success }
     end
 
     context 'when we did use required topic with multiple_usage twice' do
@@ -121,7 +121,7 @@ RSpec.describe Karafka::Schemas::ResponderUsage do
       let(:usage_count) { 2 }
       let(:multiple_usage) { true }
 
-      it { expect(subschema.call(topic_data).success?).to be_truthy }
+      it { expect(subschema.call(topic_data)).to be_success }
     end
 
     context 'when we didnt use optional topic with multiple_usage' do
@@ -129,7 +129,7 @@ RSpec.describe Karafka::Schemas::ResponderUsage do
       let(:usage_count) { 2 }
       let(:multiple_usage) { true }
 
-      it { expect(subschema.call(topic_data).success?).to be_truthy }
+      it { expect(subschema.call(topic_data)).to be_success }
     end
 
     context 'when we did use required topic without multiple_usage once' do
@@ -137,7 +137,7 @@ RSpec.describe Karafka::Schemas::ResponderUsage do
       let(:usage_count) { 1 }
       let(:multiple_usage) { false }
 
-      it { expect(subschema.call(topic_data).success?).to be_truthy }
+      it { expect(subschema.call(topic_data)).to be_success }
     end
 
     context 'when we did use required topic without multiple_usage twice' do
@@ -145,7 +145,7 @@ RSpec.describe Karafka::Schemas::ResponderUsage do
       let(:usage_count) { 2 }
       let(:multiple_usage) { false }
 
-      it { expect(subschema.call(topic_data).success?).to be_falsey }
+      it { expect(subschema.call(topic_data)).not_to be_success }
     end
   end
 end
