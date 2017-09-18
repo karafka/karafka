2f96def431fdb0cb63a6ef24d2f8532ccba53e9a
Maciej Mensfeld <maciej@mensfeld.pl>
2017-09-18 09:25:29 +0200

    predicate matches

--- spec/lib/karafka/schemas/server_cli_options_spec.rb
+++ spec/lib/karafka/schemas/server_cli_options_spec.rb
@@ -10,18 +10,18 @@ RSpec.describe Karafka::Schemas::ServerCliOptions do
   end
 
   context 'config is valid' do
-    it { expect(schema.call(config).success?).to be_truthy }
+    it { expect(schema.call(config)).to be_success }
   end
 
   context 'when we want to use consumer groups that are not defined' do
     before { config[:consumer_groups] = [rand.to_s] }
 
-    it { expect(schema.call(config).success?).to be_falsey }
+    it { expect(schema.call(config)).not_to be_success }
   end
 
   context 'when we want to use pidfile that already exists' do
     before { config[:pid] = Tempfile.new.path }
 
-    it { expect(schema.call(config).success?).to be_falsey }
+    it { expect(schema.call(config)).not_to be_success }
   end
 end
