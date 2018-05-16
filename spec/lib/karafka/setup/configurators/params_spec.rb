421f485fe20a1023a0c97fb46d30d66eb471373d
Maciej Mensfeld <maciej@mensfeld.pl>
2018-05-16 10:01:18 +0200

    Dont replace root when parsing (#336)
    * dont replace root
    
    * remarks
    
    * merge brackets

--- spec/lib/karafka/setup/configurators/params_spec.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-# @note We check the effects of this configurator as it is executed before we run specs
-RSpec.describe Karafka::Setup::Configurators::Params do
-  subject(:params_class) { Karafka::Params::Params }
-
-  it { expect(params_class).to be < Hash }
-  it { expect(params_class.included_modules.include?(Karafka::Params::Dsl)).to eq true }
-end
