21d02c0f6ca3dbeddfa13b8a58d0e5c14679ff7d
Maciej Mensfeld <maciej@mensfeld.pl>
2018-05-24 08:48:55 +0200

    spec stub

---
+++ spec/lib/karafka/instrumentation/proctitle_listener_spec.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+RSpec.describe Karafka::Instrumentation::ProctitleListener do
+  describe '#on_app_initializing' do
+    pending
+  end
+
+  describe '#on_app_running' do
+    pending
+  end
+
+  describe '#on_app_stopping' do
+    pending
+  end
+end
