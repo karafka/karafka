a0a178575b8bc427045819d194437124faa80c96
Maciej Mensfeld <maciej@mensfeld.pl>
2019-04-06 17:29:00 +0200

    invalid doc fix

--- spec/support/class_builder.rb
+++ spec/support/class_builder.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-# Class builder helps creating anonymous classes that we can use to spec our aspector
+# Class builder helps creating anonymous classes that we can use to spec our code
 # We need co create new class instances to have an "empty" and "clear" class for each spec
 # This module acts as an interface to create classes
 module ClassBuilder
