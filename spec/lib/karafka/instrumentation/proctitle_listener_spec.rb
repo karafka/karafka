d0125a2a1667cf713023d592512a7535e8d30355
Maciej Mensfeld <maciej@mensfeld.pl>
2019-04-10 12:01:56 +0200

    proctitle listener

--- spec/lib/karafka/instrumentation/proctitle_listener_spec.rb
+++ spec/lib/karafka/instrumentation/proctitle_listener_spec.rb
@@ -1,10 +1,12 @@
 # frozen_string_literal: true
 
 RSpec.describe Karafka::Instrumentation::ProctitleListener do
+  subject(:listener) { described_class.new }
+
   describe '#on_app_initializing' do
     let(:expected_title) { "karafka #{Karafka::App.config.client_id} (initializing)" }
 
-    after { described_class.on_app_initializing({}) }
+    after { listener.on_app_initializing({}) }
 
     it { expect(::Process).to receive(:setproctitle).with(expected_title) }
   end
@@ -12,7 +14,7 @@ RSpec.describe Karafka::Instrumentation::ProctitleListener do
   describe '#on_app_running' do
     let(:expected_title) { "karafka #{Karafka::App.config.client_id} (running)" }
 
-    after { described_class.on_app_running({}) }
+    after { listener.on_app_running({}) }
 
     it { expect(::Process).to receive(:setproctitle).with(expected_title) }
   end
@@ -20,7 +22,7 @@ RSpec.describe Karafka::Instrumentation::ProctitleListener do
   describe '#on_app_stopping' do
     let(:expected_title) { "karafka #{Karafka::App.config.client_id} (stopping)" }
 
-    after { described_class.on_app_stopping({}) }
+    after { listener.on_app_stopping({}) }
 
     it { expect(::Process).to receive(:setproctitle).with(expected_title) }
   end
