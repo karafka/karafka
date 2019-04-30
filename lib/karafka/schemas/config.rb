8c1e5a81d3303c272606ae6cab43d70057d01265
Maciej Mensfeld <maciej@mensfeld.pl>
2019-04-30 17:58:57 +0200

    zeitwerk setup

--- lib/karafka/schemas/config.rb
+++ lib/karafka/schemas/config.rb
@@ -1,12 +1,7 @@
 # frozen_string_literal: true
 
 module Karafka
-  # Namespace for all the validation schemas that we use to check input
   module Schemas
-    # Regexp for validating format of groups and topics
-    # @note It is not nested inside of the schema, as it is used by couple schemas
-    TOPIC_REGEXP = /\A(\w|\-|\.)+\z/.freeze
-
     # Schema with validation rules for Karafka configuration details
     # @note There are many more configuration options inside of the
     #   Karafka::Setup::Config model, but we don't validate them here as they are
