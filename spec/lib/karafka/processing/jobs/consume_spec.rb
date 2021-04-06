ea58f0f153425641fbadfc0e1fed11ee1c65c7e0
Maciej Mensfeld <maciej@mensfeld.pl>
2021-04-06 19:43:45 +0200

    consumption job spec

---
+++ spec/lib/karafka/processing/jobs/consume_spec.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+RSpec.describe_current do
+  subject(:job) { described_class.new(executor, messages) }
+
+  let(:executor) { build(:processing_executor) }
+  let(:messages) { [rand] }
+  let(:time_now) { Time.now }
+
+  before do
+    allow(Time).to receive(:now).and_return(time_now)
+    allow(executor).to receive(:consume)
+    job.call
+  end
+
+  it 'expect to run consumption on the executor with time and messages' do
+    expect(executor).to have_received(:consume).with(messages, time_now).once
+  end
+end
