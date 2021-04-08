0886f5754c3ae345154a774a01ea9f019672a87b
Maciej Mensfeld <maciej@mensfeld.pl>
2021-04-08 23:26:24 +0200

    naming cleanup

--- spec/lib/karafka/messages/message_spec.rb
+++ spec/lib/karafka/messages/message_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe_current do
   let(:headers) { { message_type: 'test' } }
 
   describe 'instance methods' do
-    subject(:params) { base_params_class.new(raw_payload, metadata) }
+    subject(:message) { base_params_class.new(raw_payload, metadata) }
 
     let(:deserializer) { ->(_) { 1 } }
     let(:metadata) do
@@ -17,32 +17,35 @@ RSpec.describe_current do
     describe '#deserialize!' do
       let(:raw_payload) { rand }
 
-      context 'when params are already deserialized' do
-        before { params.payload }
+      context 'when message payload is already deserialized' do
+        before do
+          message.payload
+          allow(message).to receive(:deserialize)
+        end
 
         it 'expect not to deserialize again and return self' do
-          expect(params).not_to receive(:deserialize)
-          expect(params.payload).to eq 1
+          expect(message.payload).to eq 1
+          expect(message).not_to have_received(:deserialize)
         end
       end
 
-      context 'when params were not yet deserializeds' do
+      context 'when message payload was not yet deserializeds' do
         let(:raw_payload) { double }
         let(:deserialized_payload) { { double => double } }
 
         before do
-          allow(params)
+          allow(message)
             .to receive(:deserialize)
             .and_return(deserialized_payload)
         end
 
         it 'expect to merge with deserialized data that is under payload key' do
-          expect(params.payload).to eq deserialized_payload
+          expect(message.payload).to eq deserialized_payload
         end
 
         it 'expect to mark as deserialized' do
-          params.payload
-          expect(params.deserialized?).to eq true
+          message.payload
+          expect(message.deserialized?).to eq true
         end
       end
 
@@ -51,19 +54,19 @@ RSpec.describe_current do
         let(:deserialized_payload) { { double => double } }
 
         before do
-          allow(params)
+          allow(message)
             .to receive(:deserialize)
             .and_raise(Karafka::Errors::BaseError)
 
           begin
-            params.payload
+            message.payload
           rescue Karafka::Errors::BaseError
             false
           end
         end
 
         it 'expect not to mark raw payload as deserialized' do
-          expect(params.deserialized?).to eq false
+          expect(message.deserialized?).to eq false
         end
       end
     end
@@ -78,12 +81,12 @@ RSpec.describe_current do
         before do
           allow(deserializer)
             .to receive(:call)
-            .with(params)
+            .with(message)
             .and_return(deserialized_payload)
         end
 
         it 'expect to return payload in a message key' do
-          expect(params.send(:deserialize)).to eq deserialized_payload
+          expect(message.send(:deserialize)).to eq deserialized_payload
         end
       end
     end
