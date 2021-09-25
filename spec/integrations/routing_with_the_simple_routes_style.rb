c54fca1f87533201b4f9ba37d0c7f3bc8ad458a2
Maciej Mensfeld <maciej@mensfeld.pl>
2021-09-25 19:33:34 +0200

    improved spec

--- spec/integrations/routing_with_the_simple_routes_style.rb
+++ spec/integrations/routing_with_the_simple_routes_style.rb
@@ -28,3 +28,27 @@ assert_equal 3, Karafka::App.consumer_groups.first.topics.size
 assert_equal 'topic1', Karafka::App.consumer_groups.first.topics.first.name
 assert_equal 'topic2', Karafka::App.consumer_groups.first.topics[1].name
 assert_equal 'topic3', Karafka::App.consumer_groups.first.topics[2].name
+
+# Re-running simple routing should just add more topics
+
+Karafka::App.routes.draw do
+  topic 'topic4' do
+    consumer Class.new
+  end
+
+  topic 'topic5' do
+    consumer Class.new
+  end
+
+  topic 'topic6' do
+    consumer Class.new
+  end
+end
+
+assert_equal 1, Karafka::App.routes.size
+assert_equal 1, Karafka::App.consumer_groups.size
+assert_equal 1, Karafka::App.subscription_groups.size
+assert_equal 6, Karafka::App.consumer_groups.first.topics.size
+assert_equal 'topic4', Karafka::App.consumer_groups.first.topics[3].name
+assert_equal 'topic5', Karafka::App.consumer_groups.first.topics[4].name
+assert_equal 'topic6', Karafka::App.consumer_groups.first.topics[5].name
