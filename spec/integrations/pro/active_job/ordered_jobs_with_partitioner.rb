f2319427523056db3063729958de1f3441ebad33
Maciej Mensfeld <maciej@mensfeld.pl>
2022-02-09 17:13:33 +0100

    add missing integration specs

--- spec/integrations/pro/active_job/ordered_jobs_with_partitioner.rb
+++ spec/integrations/pro/active_job/ordered_jobs_with_partitioner.rb
@@ -5,8 +5,58 @@
 
 setup_karafka do |config|
   config.license.token = pro_license_token
+  config.kafka['auto.offset.reset'] = 'latest'
 end
 
 setup_active_job
 
-# integrations_2_03
+# This is a special topic with 3 partitions
+TOPIC_NAME = 'integrations_2_03'
+
+Karafka::App.routes.draw do
+  consumer_group DataCollector.consumer_group do
+    active_job_topic TOPIC_NAME
+  end
+end
+
+class Job < ActiveJob::Base
+  queue_as TOPIC_NAME
+
+  karafka_options(
+    dispatch_method: :produce_sync,
+    partitioner: ->(job) { job.arguments.first[0] }
+  )
+
+  def perform(value1)
+    DataCollector.data[0] << value1
+  end
+end
+
+counts = 0
+
+# First loop kicks in before initialization of the connection and we want to publish after, that
+# is why we run it on the second run
+Karafka::App.monitor.subscribe('connection.listener.fetch_loop') do
+  counts += 1
+
+  if counts == 2
+    # We dispatch in order per partition, in case it all would go to one without partitioner or
+    # in case it would fail, the order will break
+    2.downto(0) do |partition|
+      3.times do |iteration|
+        Job.perform_later("#{partition}#{iteration}")
+      end
+    end
+  end
+end
+
+start_karafka_and_wait_until do
+  DataCollector.data[0].size >= 9
+end
+
+groups = DataCollector.data[0].group_by { |element| element[0] }
+groups.transform_values! { |group| group.map(&:to_i) }
+
+groups.each do |_, values|
+  assert_equal values.sort, values
+end
