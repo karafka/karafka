31a1bb5a6cc7e8ed392bffcd99f14d832195608a
Maciej Mensfeld <maciej@mensfeld.pl>
2022-02-21 19:50:18 +0100

    remove non lock

---
+++ spec/integrations/rails/rails7/check.rb
@@ -0,0 +1,36 @@
+# frozen_string_literal: true
+
+# Karafka+Pro should work with Rails 7 using the default setup
+
+require 'rails'
+require 'karafka'
+
+class ExampleApp < Rails::Application
+  config.eager_load = 'test'
+end
+
+Rails.configuration.middleware.delete ActionDispatch::Static
+
+dummy_boot_file = "#{Tempfile.new.path}.rb"
+FileUtils.touch(dummy_boot_file)
+ENV['KARAFKA_BOOT_FILE'] = dummy_boot_file
+
+ExampleApp.initialize!
+
+setup_karafka
+
+class Consumer < Karafka::BaseConsumer
+  def consume
+    DataCollector.data[0] << true
+  end
+end
+
+draw_routes(Consumer)
+produce(DataCollector.topic, '1')
+
+start_karafka_and_wait_until do
+  DataCollector.data[0].size >= 1
+end
+
+assert_equal 1, DataCollector.data.size
+assert_equal '7.0.2.2', Rails.version
