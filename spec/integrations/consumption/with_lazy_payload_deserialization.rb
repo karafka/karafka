83ae0808e232c3a15ba1158f634e46914f7586c7
Maciej Mensfeld <maciej@mensfeld.pl>
2022-03-19 20:00:29 +0100

    lazy param deserialization spec (#808)

---
+++ spec/integrations/consumption/with_lazy_payload_deserialization.rb
@@ -0,0 +1,39 @@
+# frozen_string_literal: true
+
+# Karafka when processing messages should deserialize only in case where we request payload to be
+# deserialized even when iterating over all the objects.
+
+setup_karafka
+
+class Deserializer
+  def call(message)
+    message.raw_payload.to_i
+  end
+end
+
+class Consumer < Karafka::BaseConsumer
+  def consume
+    messages.each do |message|
+      # This will trigger deserialization only for even numbers
+      message.payload if (message.raw_payload.to_i % 2).zero?
+
+      DataCollector.data[0] << message
+    end
+  end
+end
+
+draw_routes do
+  topic DataCollector.topic do
+    consumer Consumer
+    deserializer Deserializer.new
+  end
+end
+
+100.times { |counter| produce(DataCollector.topic, counter.to_s) }
+
+start_karafka_and_wait_until do
+  DataCollector.data[0].size >= 100
+end
+
+assert_equal 100, DataCollector.data[0].size
+assert_equal 50, DataCollector.data[0].count(&:deserialized?)
