2be5fee221a27ba221678aed72a71e22c45d58b6
Maciej Mensfeld <maciej@mensfeld.pl>
2022-03-20 10:12:19 +0100

    do not create dummy namespace

--- lib/active_job/karafka.rb
+++ lib/active_job/karafka.rb
@@ -4,6 +4,13 @@ begin
   require 'active_job'
   require_relative 'queue_adapters/karafka_adapter'
 
+  module ActiveJob
+    # Namespace for usage simplification outside of Rails where Railtie will not kick in.
+    # That way a require 'active_job/karafka' should be enough to use it
+    module Karafka
+    end
+  end
+
   # We extend routing builder by adding a simple wrapper for easier jobs topics defining
   # This needs to be extended here as it is going to be used in karafka routes, hence doing that in
   # the railtie initializer would be too late
@@ -11,11 +18,4 @@ begin
   ::Karafka::Routing::Proxy.include ::Karafka::ActiveJob::RoutingExtensions
 rescue LoadError
   # We extend ActiveJob stuff in the railtie
-end
-
-module ActiveJob
-  # Namespace for usage simplification outside of Rails where Railtie will not kick in.
-  # That way a require 'active_job/karafka' should be enough to use it
-  module Karafka
-  end
 end
