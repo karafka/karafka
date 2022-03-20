13e5b4d144df745155e9b390d2093ea589ceb41c
Maciej Mensfeld <maciej@mensfeld.pl>
2022-03-20 10:32:18 +0100

    Rails support without ActiveJob queue adapter usage (#805)

--- lib/active_job/karafka.rb
+++ lib/active_job/karafka.rb
@@ -1,20 +1,21 @@
 # frozen_string_literal: true
 
-require 'active_job'
-require 'active_job/queue_adapters'
-require 'active_job/queue_adapters/karafka_adapter'
+begin
+  require 'active_job'
+  require_relative 'queue_adapters/karafka_adapter'
 
-module ActiveJob
-  # Namespace for usage simplification outside of Rails where Railtie will not kick in.
-  # That way a require 'active_job/karafka' should be enough to use it
-  module Karafka
+  module ActiveJob
+    # Namespace for usage simplification outside of Rails where Railtie will not kick in.
+    # That way a require 'active_job/karafka' should be enough to use it
+    module Karafka
+    end
   end
+
+  # We extend routing builder by adding a simple wrapper for easier jobs topics defining
+  # This needs to be extended here as it is going to be used in karafka routes, hence doing that in
+  # the railtie initializer would be too late
+  ::Karafka::Routing::Builder.include ::Karafka::ActiveJob::RoutingExtensions
+  ::Karafka::Routing::Proxy.include ::Karafka::ActiveJob::RoutingExtensions
+rescue LoadError
+  # We extend ActiveJob stuff in the railtie
 end
-
-# We extend routing builder by adding a simple wrapper for easier jobs topics defining
-# This needs to be extended here as it is going to be used in karafka routes, hence doing that in
-# the railtie initializer would be too late
-::Karafka::Routing::Builder.include ::Karafka::ActiveJob::RoutingExtensions
-::Karafka::Routing::Proxy.include ::Karafka::ActiveJob::RoutingExtensions
-
-# We extend ActiveJob stuff in the railtie
