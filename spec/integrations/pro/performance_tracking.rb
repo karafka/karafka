72274d5e52e5480012644bffb3375a3aff89d767
Maciej Mensfeld <maciej@mensfeld.pl>
2022-04-22 19:51:54 +0200

    align times to compensate for slow ci

--- spec/integrations/pro/performance_tracking.rb
+++ spec/integrations/pro/performance_tracking.rb
@@ -48,6 +48,6 @@ TOPICS.each do |topic_name|
   message_speed = MESSAGE_SPEED.fetch(topic_name)
 
   assert_equal true, p95 >= message_speed, "Expected #{p95} to be gteq: #{message_speed}"
-  # We add 4ms to compensate for slow ci
-  assert_equal true, p95 <= message_speed + 4, "Expected #{p95} to be lteq: #{message_speed + 4}"
+  # We add 10ms to compensate for slow ci
+  assert_equal true, p95 <= message_speed + 10, "Expected #{p95} to be lteq: #{message_speed + 10}"
 end
