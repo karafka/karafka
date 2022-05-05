6081f5f92aac155db0aba13131c8d5a213372169
Maciej Mensfeld <maciej@mensfeld.pl>
2022-05-05 17:53:06 +0200

    allow time based pause

--- spec/lib/karafka/time_trackers/pause_spec.rb
+++ spec/lib/karafka/time_trackers/pause_spec.rb
@@ -222,4 +222,82 @@ RSpec.describe_current do
       it { expect(tracker.count).to eq(0) }
     end
   end
+
+  context 'when we define a custom manual pause time' do
+    let(:timeout) { 100 }
+    let(:max_timeout) { 100 }
+    let(:exponential_backoff) { false }
+
+    context 'when pause tracker is created' do
+      it { expect(tracker.expired?).to eq(true) }
+      it { expect(tracker.paused?).to eq(false) }
+      it { expect(tracker.count).to eq(0) }
+    end
+
+    context 'when immediately after paused' do
+      before { tracker.pause(1) }
+
+      it { expect(tracker.expired?).to eq(false) }
+      it { expect(tracker.paused?).to eq(true) }
+      it { expect(tracker.count).to eq(1) }
+    end
+
+    context 'when paused and manually expired' do
+      before do
+        tracker.pause(1_000)
+        tracker.expire
+      end
+
+      it { expect(tracker.expired?).to eq(true) }
+      it { expect(tracker.paused?).to eq(true) }
+      it { expect(tracker.count).to eq(1) }
+    end
+
+    context 'when not paused over timeout nor max timeout' do
+      before do
+        tracker.pause(2)
+        sleep(0.001)
+      end
+
+      it { expect(tracker.expired?).to eq(false) }
+      it { expect(tracker.paused?).to eq(true) }
+      it { expect(tracker.count).to eq(1) }
+    end
+
+    context 'when paused over max timeout' do
+      before do
+        tracker.pause(1)
+        sleep(0.006)
+      end
+
+      it { expect(tracker.expired?).to eq(true) }
+      it { expect(tracker.paused?).to eq(true) }
+      it { expect(tracker.count).to eq(1) }
+    end
+
+    context 'when paused over timeout and resumed' do
+      before do
+        tracker.pause(1)
+        sleep(0.01)
+        tracker.resume
+      end
+
+      it { expect(tracker.expired?).to eq(true) }
+      it { expect(tracker.paused?).to eq(false) }
+      it { expect(tracker.count).to eq(1) }
+    end
+
+    context 'when paused over timeout, resumed and reset' do
+      before do
+        tracker.pause(1)
+        sleep(0.01)
+        tracker.resume
+        tracker.reset
+      end
+
+      it { expect(tracker.expired?).to eq(true) }
+      it { expect(tracker.paused?).to eq(false) }
+      it { expect(tracker.count).to eq(0) }
+    end
+  end
 end
