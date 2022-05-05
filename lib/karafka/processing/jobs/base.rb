80d15f2b0748b5ea5ce5449f48c3f5cfc0c7030e
Maciej Mensfeld <maciej@mensfeld.pl>
2022-05-05 15:31:44 +0200

    Introduce notion of non-blocking jobs (#844)

--- lib/karafka/processing/jobs/base.rb
+++ lib/karafka/processing/jobs/base.rb
@@ -5,6 +5,8 @@ module Karafka
     # Namespace for all the jobs that are suppose to run in workers.
     module Jobs
       # Base class for all the jobs types that are suppose to run in workers threads.
+      # Each job can have 3 main entry-points: `#prepare`, `#call` and `#teardown`
+      # Only `#call` is required.
       class Base
         extend Forwardable
 
@@ -12,6 +14,29 @@ module Karafka
         def_delegators :executor, :id, :group_id
 
         attr_reader :executor
+
+        def initialize
+          # Every job by default is blocking and blocks the jobs queue until finished
+          @non_blocking = false
+        end
+
+        # When redefined can run any code that should run before executing the proper code
+        def prepare; end
+
+        # When redefined can run any code that should run after executing the proper code
+        def teardown; end
+
+        # @return [Boolean] is this a non-blocking job
+        # @note Blocking job is a job, that will cause the job queue to wait until it is finished
+        #   before removing the lock on new jobs being added
+        def non_blocking?
+          @non_blocking
+        end
+
+        # Marks this job as no longer blocking
+        def unblock!
+          @non_blocking = true
+        end
       end
     end
   end
--- spec/lib/karafka/processing/jobs/base_spec.rb
+++ spec/lib/karafka/processing/jobs/base_spec.rb
@@ -1,4 +1,15 @@
 # frozen_string_literal: true
 
-# Empty stub for Diffend
-RSpec.describe_current {}
+RSpec.describe_current do
+  describe '#non_blocking?' do
+    it 'expect all the newly created jobs to be blocking' do
+      expect(described_class.new.non_blocking?).to eq(false)
+    end
+
+    it 'expect job that is marked as unblocked not to be blocking' do
+      job = described_class.new
+      job.unblock!
+      expect(job.non_blocking?).to eq(true)
+    end
+  end
+end
