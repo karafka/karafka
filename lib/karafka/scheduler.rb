13d114e212420322ad761d1531a86b8a68910503
Maciej Mensfeld <maciej@mensfeld.pl>
2022-05-23 17:00:29 +0200

    remarks

--- lib/karafka/scheduler.rb
+++ lib/karafka/scheduler.rb
@@ -5,12 +5,10 @@ module Karafka
   class Scheduler
     # Yields jobs in the fifo order
     #
-    # @param [Array<Karafka::Processing::Jobs::Base>] jobs we want to schedule
+    # @param jobs_array [Array<Karafka::Processing::Jobs::Base>] jobs we want to schedule
     # @yieldparam [Karafka::Processing::Jobs::Base] job we want to enqueue
-    def call(jobs_array)
-      jobs_array.each do |job|
-        yield job
-      end
+    def call(jobs_array, &block)
+      jobs_array.each(&block)
     end
   end
 end
