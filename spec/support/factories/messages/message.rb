2875ab0e17b6361629c7eea102eb4d880778c4b9
Maciej Mensfeld <maciej@mensfeld.pl>
2022-05-23 16:56:53 +0200

    make scheduler work with jobs instead of messages

--- spec/support/factories/messages/message.rb
+++ spec/support/factories/messages/message.rb
@@ -4,8 +4,13 @@ FactoryBot.define do
   factory :messages_message, class: 'Karafka::Messages::Message' do
     skip_create
 
+    transient do
+      sequence(:topic) { |nr| "topic#{nr}" }
+      partition { 0 }
+    end
+
     raw_payload { '{}' }
-    metadata { build(:messages_metadata) }
+    metadata { build(:messages_metadata, topic: topic, partition: partition) }
 
     initialize_with do
       new(raw_payload, metadata)
