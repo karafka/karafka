9ff2688fead1d9bb20eb1e71e5517e63c300218d
Maciej Mensfeld <maciej@mensfeld.pl>
2022-05-29 22:16:03 +0200

    more spec

--- spec/lib/karafka/connection/listeners_batch_spec.rb
+++ spec/lib/karafka/connection/listeners_batch_spec.rb
@@ -1,5 +1,20 @@
 # frozen_string_literal: true
 
 RSpec.describe_current do
-  pending
+  subject(:batch) { described_class.new(jobs_queue) }
+
+  let(:jobs_queue) { Karafka::Processing::JobsQueue.new }
+  let(:subscription_group) { build(:routing_subscription_group) }
+
+  describe '#each' do
+    before do
+      allow(Karafka::App).to receive(:subscription_groups).and_return([subscription_group])
+    end
+
+    it 'expect to yield each listener' do
+      batch.each do |listener|
+        expect(listener).to be_a(Karafka::Connection::Listener)
+      end
+    end
+  end
 end
