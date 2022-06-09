e6aa5333d77e647eb889aff7527af3c04d77799a
Maciej Mensfeld <maciej@mensfeld.pl>
2022-06-09 22:58:38 +0200

    add missing specs

--- spec/lib/karafka/processing/jobs_builder_spec.rb
+++ spec/lib/karafka/processing/jobs_builder_spec.rb
@@ -1,15 +1,19 @@
 # frozen_string_literal: true
 
 RSpec.describe_current do
+  subject(:builder) { described_class.new }
+
+  let(:executor) { build(:processing_executor) }
+
   describe '#consume' do
-    pending
+    it { expect(builder.consume(executor, [])).to be_a(Karafka::Processing::Jobs::Consume) }
   end
 
   describe '#revoked' do
-    pending
+    it { expect(builder.revoked(executor)).to be_a(Karafka::Processing::Jobs::Revoked) }
   end
 
   describe '#shutdown' do
-    pending
+    it { expect(builder.shutdown(executor)).to be_a(Karafka::Processing::Jobs::Shutdown) }
   end
 end
