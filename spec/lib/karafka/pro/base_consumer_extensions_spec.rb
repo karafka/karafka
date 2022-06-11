64aefed98425a8a040721b98c1aca88612d51dcd
Maciej Mensfeld <maciej@mensfeld.pl>
2022-06-11 11:58:54 +0200

    missing specs

--- spec/lib/karafka/pro/base_consumer_extensions_spec.rb
+++ spec/lib/karafka/pro/base_consumer_extensions_spec.rb
@@ -3,11 +3,21 @@
 require 'karafka/pro/base_consumer_extensions'
 
 RSpec.describe_current do
-  describe '#on_revoked' do
-    pending
+  subject(:consumer) do
+    Karafka::BaseConsumer.new.tap do |instance|
+      instance.singleton_class.include Karafka::Pro::BaseConsumerExtensions
+    end
   end
 
-  describe '#revoked?' do
-    pending
+  describe '#revoked? and #on_revoked' do
+    context 'when partition was not revoked' do
+      it { expect(consumer.revoked?).to eq(false) }
+    end
+
+    context 'when partition was revoked' do
+      before { consumer.on_revoked }
+
+      it { expect(consumer.revoked?).to eq(true) }
+    end
   end
 end
