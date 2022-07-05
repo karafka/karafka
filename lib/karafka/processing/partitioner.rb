7324aa0bdac5f44599206c229870c9fc78ec7202
Maciej Mensfeld <maciej@mensfeld.pl>
2022-07-05 15:46:17 +0200

    remarks

--- lib/karafka/processing/partitioner.rb
+++ lib/karafka/processing/partitioner.rb
@@ -5,15 +5,16 @@ module Karafka
     # Basic partitioner for work division
     # It does not divide any work.
     class Partitioner
+      # @param subscription_group [Karafka::Routing::SubscriptionGroup] subscription group
       def initialize(subscription_group)
         @subscription_group = subscription_group
       end
 
-      # @param topic [String] topic name
+      # @param _topic [String] topic name
       # @param messages [Array<Karafka::Messages::Message>] karafka messages
       # @yieldparam [Integer] group id
       # @yieldparam [Array<Karafka::Messages::Message>] karafka messages
-      def call(topic, messages)
+      def call(_topic, messages)
         yield(0, messages)
       end
     end
