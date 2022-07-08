23dd5b9c2425a5b38aadfa2acf8d31983183ff51
Maciej Mensfeld <maciej@mensfeld.pl>
2022-07-08 22:37:16 +0200

    Extra spec illustrating headers usage + readme update (#896)

---
+++ spec/integrations/consumption/loop_with_messages_with_headers.rb
@@ -0,0 +1,39 @@
+# frozen_string_literal: true
+
+# Karafka should be able to consume messages in a loop
+# Messages can have headers that should be accessible to use
+
+setup_karafka
+
+class Consumer < Karafka::BaseConsumer
+  def consume
+    messages.each do |message|
+      DataCollector[:headers] << message.headers.fetch('iteration').to_i
+    end
+
+    producer.produce_sync(
+      topic: DataCollector.topic,
+      payload: rand.to_s,
+      headers: { 'iteration' => (messages.last.headers.fetch('iteration').to_i + 1).to_s }
+    )
+  end
+end
+
+draw_routes(Consumer)
+
+Karafka.producer.produce_sync(
+  topic: DataCollector.topic,
+  payload: rand.to_s,
+  headers: { 'iteration' => '0' }
+)
+
+start_karafka_and_wait_until do
+  DataCollector[:headers].size >= 20
+end
+
+i = 0
+
+DataCollector[:headers].each do |iteration|
+  assert_equal i, iteration
+  i += 1
+end
