106aab9a1e045911274d198a56185fe5da84f7ef
Maciej Mensfeld <maciej@mensfeld.pl>
2022-08-22 13:03:06 +0200

    make poll real time independent

--- spec/lib/karafka/time_trackers/poll_spec.rb
+++ spec/lib/karafka/time_trackers/poll_spec.rb
@@ -1,19 +1,20 @@
 # frozen_string_literal: true
 
 RSpec.describe_current do
-  subject(:tracker) { described_class.new(200) }
+  subject(:tracker) { described_class.new(1_000) }
 
   context 'when we still have time after 2 ms and it is first attempt' do
     before do
+      allow(::Process).to receive(:clock_gettime).and_return(1, 1.002)
+
       tracker.start
-      sleep 0.002
       tracker.checkpoint
     end
 
     it { expect(tracker.exceeded?).to eq(false) }
     it { expect(tracker.retryable?).to eq(true) }
     # Compensate for slow CI
-    it { expect(tracker.remaining).to  be_within(5).of(197) }
+    it { expect(tracker.remaining).to  be_within(50).of(997) }
     it { expect(tracker.attempts).to eq(1) }
 
     context 'when needing to backoff' do
@@ -28,8 +29,9 @@ RSpec.describe_current do
 
   context 'when we no longer have time after first attempt' do
     before do
+      allow(::Process).to receive(:clock_gettime).and_return(1, 2)
+
       tracker.start
-      sleep 0.2
       tracker.checkpoint
     end
 
@@ -50,16 +52,17 @@ RSpec.describe_current do
 
   context 'when we have several attempts each within time range but exceeding retry' do
     before do
+      allow(::Process).to receive(:clock_gettime).and_return(1, 1.25, 1.5, 1.75, 2.0, 2.25)
+
       3.times do
         tracker.start
-        sleep 0.005
         tracker.checkpoint
       end
     end
 
     it { expect(tracker.exceeded?).to eq(false) }
     it { expect(tracker.retryable?).to eq(false) }
-    it { expect(tracker.remaining).to be_within(10).of(185) }
+    it { expect(tracker.remaining).to be_within(50).of(250) }
     it { expect(tracker.attempts).to eq(3) }
 
     context 'when needing to backoff' do
@@ -74,16 +77,15 @@ RSpec.describe_current do
 
   context 'when we do not have enough time to backoff' do
     before do
+      allow(::Process).to receive(:clock_gettime).and_return(1, 1.995)
+
       tracker.start
-
-      sleep 0.1995
-
       tracker.checkpoint
     end
 
     it { expect(tracker.exceeded?).to eq(false) }
     it { expect(tracker.retryable?).to eq(false) }
-    it { expect(tracker.remaining).to be_within(1).of(0) }
+    it { expect(tracker.remaining).to be_within(4).of(5) }
     it { expect(tracker.attempts).to eq(1) }
 
     context 'when needing to backoff' do
