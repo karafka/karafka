daa5f92c5a7569fa953f67bc57b1be86632be217
Maciej Mensfeld <maciej@mensfeld.pl>
2022-08-22 13:26:01 +0200

    improve spec stability

--- spec/integrations/pro/rails/active_job/virtual_partitions/from_earliest.rb
+++ spec/integrations/pro/rails/active_job/virtual_partitions/from_earliest.rb
@@ -11,13 +11,11 @@ end
 
 setup_active_job
 
-picks = Array.new(1_000){ |i| i % 5 }
-
 draw_routes do
   consumer_group DT.consumer_group do
     active_job_topic DT.topic do
       # We do not publish any details with this job, thus we do a random work distribution
-      virtual_partitioner ->(_) { picks.sample }
+      virtual_partitioner ->(_) { (0..4).to_a.sample }
     end
   end
 end
