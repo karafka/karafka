9c1b350091da5eb02c56d768fd54c5324e917ecd
Maciej Mensfeld <maciej@mensfeld.pl>
2022-10-03 22:28:19 +0200

    wip

--- lib/karafka/patches/rdkafka/bindings.rb
+++ lib/karafka/patches/rdkafka/bindings.rb
@@ -18,21 +18,46 @@ module Karafka
         RebalanceCallback = FFI::Function.new(
           :void, %i[pointer int pointer pointer]
         ) do |client_ptr, code, partitions_ptr, opaque_ptr|
-          case code
-          when RD_KAFKA_RESP_ERR__ASSIGN_PARTITIONS
-            ::Rdkafka::Bindings.rd_kafka_assign(client_ptr, partitions_ptr)
-          when RD_KAFKA_RESP_ERR__REVOKE_PARTITIONS
-            ::Rdkafka::Bindings.rd_kafka_commit(client_ptr, nil, false)
-            ::Rdkafka::Bindings.rd_kafka_assign(client_ptr, FFI::Pointer::NULL)
+          p 'a'
+
+          if ::Rdkafka::Bindings.rd_kafka_rebalance_protocol(client_ptr) == 'COOPERATIVE'
+            p 'c'
+            p [code, RD_KAFKA_RESP_ERR__ASSIGN_PARTITIONS, RD_KAFKA_RESP_ERR__REVOKE_PARTITIONS]
+            p ::Rdkafka::Consumer::TopicPartitionList.from_native_tpl(partitions_ptr).freeze
+            case code
+            when RD_KAFKA_RESP_ERR__ASSIGN_PARTITIONS
+              ::Rdkafka::Bindings.rd_kafka_incremental_assign(client_ptr, partitions_ptr)
+            when RD_KAFKA_RESP_ERR__REVOKE_PARTITIONS
+          #    ::Rdkafka::Bindings.rd_kafka_commit(client_ptr, nil, false)
+              ::Rdkafka::Bindings.rd_kafka_incremental_unassign(client_ptr, partitions_ptr)
+            else
+              ::Rdkafka::Bindings.rd_kafka_assign(client_ptr, FFI::Pointer::NULL)
+            end
+
+            opaque = ::Rdkafka::Config.opaques[opaque_ptr.to_i]
+            return unless opaque
+
+            tpl = ::Rdkafka::Consumer::TopicPartitionList.from_native_tpl(partitions_ptr).freeze
+            consumer = ::Rdkafka::Consumer.new(client_ptr)
           else
-            ::Rdkafka::Bindings.rd_kafka_assign(client_ptr, FFI::Pointer::NULL)
+            p 'x'
+            case code
+            when RD_KAFKA_RESP_ERR__ASSIGN_PARTITIONS
+              ::Rdkafka::Bindings.rd_kafka_assign(client_ptr, partitions_ptr)
+            when RD_KAFKA_RESP_ERR__REVOKE_PARTITIONS
+              ::Rdkafka::Bindings.rd_kafka_commit(client_ptr, nil, false)
+              ::Rdkafka::Bindings.rd_kafka_assign(client_ptr, FFI::Pointer::NULL)
+            else
+              ::Rdkafka::Bindings.rd_kafka_assign(client_ptr, FFI::Pointer::NULL)
+            end
+
+            opaque = ::Rdkafka::Config.opaques[opaque_ptr.to_i]
+            return unless opaque
+
+            tpl = ::Rdkafka::Consumer::TopicPartitionList.from_native_tpl(partitions_ptr).freeze
+            consumer = ::Rdkafka::Consumer.new(client_ptr)
           end
 
-          opaque = ::Rdkafka::Config.opaques[opaque_ptr.to_i]
-          return unless opaque
-
-          tpl = ::Rdkafka::Consumer::TopicPartitionList.from_native_tpl(partitions_ptr).freeze
-          consumer = ::Rdkafka::Consumer.new(client_ptr)
 
           begin
             case code
@@ -64,3 +89,7 @@ end
   'RebalanceCallback',
   Karafka::Patches::Rdkafka::Bindings::RebalanceCallback
 )
+
+::Rdkafka::Bindings.attach_function :rd_kafka_rebalance_protocol, [:pointer], :string
+::Rdkafka::Bindings.attach_function :rd_kafka_incremental_assign, [:pointer, :pointer], :string
+::Rdkafka::Bindings.attach_function :rd_kafka_incremental_unassign, [:pointer, :pointer], :string
