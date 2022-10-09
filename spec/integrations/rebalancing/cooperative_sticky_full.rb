f9afdb3e10d63dcd21fa2a8a73fa28dcf8bc7f1a
Maciej Mensfeld <maciej@mensfeld.pl>
2022-10-09 09:30:11 +0200

    #1053 cooperative-sticky-full-spec

---
+++ spec/integrations/rebalancing/cooperative_sticky_full.rb
@@ -0,0 +1,65 @@
+# frozen_string_literal: true
+
+# Karafka will run a full rebalance in case we use cooperative-sticky but force commit when
+# rebalance happens. This is how `librdkafka` works
+
+setup_karafka(allow_errors: true) do |config|
+  config.kafka[:'partition.assignment.strategy'] = 'cooperative-sticky'
+end
+
+create_topic(partitions: 5)
+
+Thread.new do
+  loop do
+    begin
+      produce(DT.topic, '1', partition: 0)
+      produce(DT.topic, '2', partition: 1)
+      produce(DT.topic, '3', partition: 2)
+      produce(DT.topic, '4', partition: 3)
+      produce(DT.topic, '5', partition: 4)
+    rescue WaterDrop::Errors::ProducerClosedError
+      break
+    end
+
+    sleep(1)
+  end
+end
+
+class Consumer < Karafka::BaseConsumer
+  def consume
+    mark_as_consumed! messages.last
+  end
+
+  def on_revoked
+    DT[:revoked] << messages.metadata.partition
+  end
+end
+
+draw_routes(Consumer)
+
+consumer = setup_rdkafka_consumer(
+  'partition.assignment.strategy': 'cooperative-sticky'
+)
+
+other = Thread.new do
+  sleep(10)
+  consumer.subscribe(DT.topic)
+
+  consumer.each do |message|
+    DT[:picked] << message.partition
+
+    break if DT[:picked].uniq.size >= 2
+  end
+
+  sleep(5)
+
+  consumer.close
+end
+
+start_karafka_and_wait_until do
+  other.join
+
+  true
+end
+
+assert_equal [0, 1, 2, 3, 4], DT.data[:revoked].sort
