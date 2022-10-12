7a9f10be31df69bb6543bb5f0d6448ade87b0f55
Maciej Mensfeld <maciej@mensfeld.pl>
2022-10-12 18:38:51 +0200

    compression specs (#1062)

---
+++ spec/integrations/consumption/compressions/using_snappy.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+setup_karafka do |config|
+  config.kafka[:'compression.codec'] = 'snappy'
+end
+
+class Consumer < Karafka::BaseConsumer
+  def consume
+    messages.each do |message|
+      DT[0] << message.raw_payload
+    end
+  end
+end
+
+draw_routes(Consumer)
+
+elements = DT.uuids(10)
+produce_many(DT.topic, elements)
+
+start_karafka_and_wait_until do
+  DT[0].size >= 10
+end
