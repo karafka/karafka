8ad6b695e91e6f706a8075e0e34272b5a5f2bb9f
Maciej Mensfeld <maciej@mensfeld.pl>
2022-10-16 12:29:56 +0200

    compensate for first request unpredictability

--- spec/integrations/rebalancing/exceeding_max_poll_with_late_commit_spec.rb
+++ spec/integrations/rebalancing/exceeding_max_poll_with_late_commit_spec.rb
@@ -1,13 +1,14 @@
 # frozen_string_literal: true
 
 # When processing beyond the poll interval, with slower offset commit, we will restart processing
+# and there should be duplicated messages.
 
 setup_karafka(
   # Allow max poll interval error as it is expected to be reported in this spec
   allow_errors: %w[connection.client.poll.error]
 ) do |config|
   config.max_messages = 5
-  # We set it here that way not too wait too long on stuff
+  # We set it here, that way we won't too wait too long on stuff
   config.kafka[:'max.poll.interval.ms'] = 10_000
   config.kafka[:'session.timeout.ms'] = 10_000
   config.kafka[:'auto.commit.interval.ms'] = 60_000
@@ -23,7 +24,7 @@ class Consumer < Karafka::BaseConsumer
       mark_as_consumed message
     end
 
-    sleep(15)
+    sleep(15) if messages.count > 1
   end
 end
 
@@ -32,7 +33,7 @@ draw_routes(Consumer)
 produce_many(DT.topic, DT.uuids(100))
 
 start_karafka_and_wait_until do
-  DT[:done].size >= 2
+  DT[:done].size >= 10
 end
 
-assert_equal 1, DT[:done].uniq.size
+assert DT[:done].size != DT[:done].uniq.size
