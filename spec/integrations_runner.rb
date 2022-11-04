827ea7cf0ea1813239956044fb6326c1ee52aae8
ojab <ojab@ojab.ru>
2022-11-04 16:33:35 +0000

    TMP

--- spec/integrations_runner.rb
+++ spec/integrations_runner.rb
@@ -1,6 +1,9 @@
 require 'bundler'
 
-require 'rails' if Bundler.require(:default, :test, :integrations).any? { |gem| gem.name == 'railties' }
+Bundler.require(:default, :test, :integrations)
+
+require 'rails' if Bundler.locked_gems.specs.any? { |spec| spec.name == 'railties' }
+require 'active_job/railtie' if Bundler.locked_gems.specs.any? { |spec| spec.name == 'activejob' }
 
 require_relative './integrations_helper'
 
