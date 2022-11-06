ff9548d9dc3f242893c2a666a6529d2389847f9c
Maciej Mensfeld <maciej@mensfeld.pl>
2022-11-06 09:52:30 +0100

    coordinator count

---
+++ spec/integrations/pro/consumption/with_never_ending_error.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+# When doing work with error, we should slowly increase the attempt count
+
+setup_karafka(allow_errors: %w[consumer.consume.error]) do |config|
+  config.max_messages = 20
+  config.license.token = pro_license_token
+end
+
+class Consumer < Karafka::Pro::BaseConsumer
+  def consume
+    DT[:attempts] << coordinator.pause_tracker.attempt
+
+    raise StandardError
+  end
+end
+
+draw_routes(Consumer)
+
+elements = DT.uuids(100)
+produce_many(DT.topic, elements)
+
+start_karafka_and_wait_until do
+  DT[:attempts].size >= 20
+end
+
+assert_equal [], (1..20).to_a - DT[:attempts], DT[:attempts]
