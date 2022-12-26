ddd1ba60dbf1de72d4790c2ae2ea9c947c27eb30
Maciej Mensfeld <maciej@mensfeld.pl>
2022-12-26 14:39:11 +0100

    spec for admin coop with disabled topics (#1242)

---
+++ spec/integrations/admin/read_topic_with_custom_deserializer_spec.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+# When using read_topic with topic that is inactive, the deserializer resolution should work
+# and properly defined deserializer should be used
+
+setup_karafka
+
+class Custom
+  def call(_message)
+    1
+  end
+end
+
+draw_routes do
+  topic DT.topic do
+    active false
+    deserializer Custom.new
+  end
+end
+
+create_topic(name: DT.topic)
+
+produce(DT.topic, '10')
+
+messages = Karafka::Admin.read_topic(DT.topic, 0, 1)
+assert_equal 1, messages.last.payload
