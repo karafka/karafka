cdfa8610470a602836bb4cf098301ea4651b0dad
Maciej Mensfeld <maciej@mensfeld.pl>
2023-01-09 16:55:07 +0100

    add post integration verification layer

---
+++ spec/integrations/pro/licensing/post_checksum_verification_poro/verify_spec.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+# Run the verification script post install to make sure it works as expected
+
+cmd = <<~CMD
+  MODE=after \
+  KARAFKA_PRO_USERNAME='#{ENV['KARAFKA_PRO_USERNAME']}' \
+  KARAFKA_PRO_PASSWORD='#{ENV['KARAFKA_PRO_PASSWORD']}' \
+  KARAFKA_PRO_VERSION='#{ENV['KARAFKA_PRO_VERSION']}' \
+  KARAFKA_PRO_LICENSE_CHECKSUM='#{ENV['KARAFKA_PRO_LICENSE_CHECKSUM']}' \
+  #{ENV['KARAFKA_GEM_DIR']}/bin/verify_license_integrity
+CMD
+
+exit 1 unless cmd.include?('verification result: Success')
