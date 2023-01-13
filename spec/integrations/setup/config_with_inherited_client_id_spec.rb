4040445bfa5261e37e4515f3bdb487be288fed6d
Maciej Mensfeld <maciej@mensfeld.pl>
2023-01-13 17:39:17 +0100

    specs

---
+++ spec/integrations/setup/config_with_inherited_client_id_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+# Karafka should use client_id for kafka.client.id if kafka.client.id is not provided
+
+guarded = []
+
+setup_karafka do |config|
+  config.client_id = 'test-app'
+end
+
+assert_equal Karafka::App.config.kafka[:'client.id'], 'test-app'
---
+++ spec/integrations/setup/config_with_overwritten_client_id_spec.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+# Karafka should not use client_id for kafka.client.id if kafka.client.id is provided
+
+guarded = []
+
+setup_karafka do |config|
+  config.client_id = 'test-app'
+  config.kafka[:'client.id'] = 'alternative-name'
+end
+
+assert_equal Karafka::App.config.kafka[:'client.id'], 'alternative-name'
