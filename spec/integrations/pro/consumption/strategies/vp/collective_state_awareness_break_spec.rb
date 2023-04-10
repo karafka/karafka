d9d31e1ed84435ba6b4d12dc648e092bc636d0c3
Maciej Mensfeld <maciej@mensfeld.pl>
2023-04-10 16:57:40 +0200

    remarks

--- spec/integrations/pro/consumption/strategies/vp/collective_state_awareness_break_spec.rb
+++ spec/integrations/pro/consumption/strategies/vp/collective_state_awareness_break_spec.rb
@@ -62,7 +62,7 @@ draw_routes do
   end
 end
 
-produce_many(DT.topic, Array.new(100) { |i| i.to_s })
+produce_many(DT.topic, Array.new(100, &:to_s))
 
 start_karafka_and_wait_until do
   DT[:failing].size >= 9
