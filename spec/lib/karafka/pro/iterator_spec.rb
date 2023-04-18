ed47bacfbcbad555dcbcfd93fdb756dade9642fc
Maciej Mensfeld <maciej@mensfeld.pl>
2023-04-18 13:25:07 +0200

    remarks

--- spec/lib/karafka/pro/iterator_spec.rb
+++ spec/lib/karafka/pro/iterator_spec.rb
@@ -1,7 +1,14 @@
 # frozen_string_literal: true
 
+# More scenarios covered by the integrations - here just the basics
 RSpec.describe_current do
-  subject(:iterator) { described_class }
+  subject(:iterator) { described_class.new(topic) }
 
-  pending
+  let(:topic) { SecureRandom.uuid }
+
+  before { Karafka::Admin.create_topic(topic, 2, 1) }
+
+  it 'expect to start and stop iterator' do
+    iterator.each {}
+  end
 end
