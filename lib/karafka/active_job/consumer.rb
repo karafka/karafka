0f02081b1a77db29f807f1fe51a520b0f2d705d4
Federico Moretti <federico@goldbelly.com>
2023-04-28 18:29:15 -0400

    Updated active job consumer.

--- lib/karafka/active_job/consumer.rb
+++ lib/karafka/active_job/consumer.rb
@@ -24,20 +24,26 @@ module Karafka
       #
       # @param job_message [Karafka::Messages::Message] message with active job
       def consume_job(job_message)
+        with_deserialized_job(job_message) do |job|
+          tags.add(:job_class, job['job_class'])
+
+          payload = { caller: self, job: job, message: job_message }
+
+          # We publish both to make it consistent with `consumer.x` events
+          Karafka.monitor.instrument('active_job.consume', payload)
+          Karafka.monitor.instrument('active_job.consumed', payload) do
+            ::ActiveJob::Base.execute(job)
+          end
+        end
+      end
+
+      # @param job_message [Karafka::Messages::Message] message with active job
+      # @param block [Proc] block that should be executed with the deserialized job
+      def with_deserialized_job(job_message, &block)
         # We technically speaking could set this as deserializer and reference it from the
         # message instead of using the `#raw_payload`. This is not done on purpose to simplify
         # the ActiveJob setup here
-        job = ::ActiveSupport::JSON.decode(job_message.raw_payload)
-
-        tags.add(:job_class, job['job_class'])
-
-        payload = { caller: self, job: job, message: job_message }
-
-        # We publish both to make it consistent with `consumer.x` events
-        Karafka.monitor.instrument('active_job.consume', payload)
-        Karafka.monitor.instrument('active_job.consumed', payload) do
-          ::ActiveJob::Base.execute(job)
-        end
+        yield ::ActiveSupport::JSON.decode(job_message.raw_payload)
       end
     end
   end
