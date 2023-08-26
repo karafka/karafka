4cb69e7d4debaf62199cb04952ec5b5f73fd8193
Maciej Mensfeld <maciej@mensfeld.pl>
2023-08-26 20:31:43 +0200

    remarks

--- lib/karafka/pro/routing/features/active_job.rb
+++ lib/karafka/pro/routing/features/active_job.rb
@@ -15,17 +15,10 @@ module Karafka
   module Pro
     module Routing
       module Features
-        # Dynamic topics builder feature.
-        #
-        # Allows you to define patterns in routes that would then automatically subscribe and
-        # start consuming new topics.
-        #
-        # This feature works by injecting a topic that represents a regexp subscription (matcher)
-        # that at the same time holds the builder block for full config of a newly detected topic.
-        #
-        # We inject a virtual topic to hold settings but also to be able to run validations
-        # during boot to ensure consistency of the pattern base setup.
-        class Patterns < Base
+        # Small extension to make ActiveJob work with pattern matching.
+        # Since our `#active_job_topic` is just a topic wrapper, we can introduce a similar
+        # `#active_job_pattern` to align with pattern building
+        class ActiveJob < Base
         end
       end
     end
---
+++ lib/karafka/pro/routing/features/active_job/builder.rb
@@ -0,0 +1,41 @@
+# frozen_string_literal: true
+
+# This Karafka component is a Pro component under a commercial license.
+# This Karafka component is NOT licensed under LGPL.
+#
+# All of the commercial components are present in the lib/karafka/pro directory of this
+# repository and their usage requires commercial license agreement.
+#
+# Karafka has also commercial-friendly license, commercial support and commercial components.
+#
+# By sending a pull request to the pro components, you are agreeing to transfer the copyright of
+# your code to Maciej Mensfeld.
+
+module Karafka
+  module Pro
+    module Routing
+      module Features
+        class ActiveJob < Base
+          module Builder
+            # This method simplifies routes definition for ActiveJob patterns / queues by
+            # auto-injecting the consumer class and other things needed
+            #
+            # @param regexp [Regexp] activejob regexp pattern
+            # @param block [Proc] block that we can use for some extra configuration
+            def active_job_pattern(regexp, &block)
+              pattern(regexp) do
+                consumer App.config.internal.active_job.consumer_class
+                active_job true
+                manual_offset_management true
+
+                next unless block
+
+                instance_eval(&block)
+              end
+            end
+          end
+        end
+      end
+    end
+  end
+end
