df3f609a9cf3ea13ad0b6a21cb6f038d556e86a1
Maciej Mensfeld <maciej@mensfeld.pl>
2023-08-29 17:32:51 +0200

    improve stability

--- spec/integrations/pro/consumption/strategies/dlq/ftr_lrj_mom/non_recoverable_moving_forward_marking_spec.rb
+++ spec/integrations/pro/consumption/strategies/dlq/ftr_lrj_mom/non_recoverable_moving_forward_marking_spec.rb
@@ -9,7 +9,7 @@ end
 Karafka.monitor.subscribe(Listener.new)
 
 setup_karafka(allow_errors: true) do |config|
-  config.max_messages = 10
+  config.max_messages = 1
   config.kafka[:'max.poll.interval.ms'] = 10_000
   config.kafka[:'session.timeout.ms'] = 10_000
 end
