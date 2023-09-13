158567bdd26b33658a86fdf821e1b94e78a9554b
Maciej Mensfeld <maciej@mensfeld.pl>
2023-09-13 21:01:35 +0200

    remarks

--- lib/karafka/messages/builders/batch_metadata.rb
+++ lib/karafka/messages/builders/batch_metadata.rb
@@ -26,12 +26,32 @@ module Karafka
               topic: topic.name,
               # We go with the assumption that the creation of the whole batch is the last message
               # creation time
-              created_at: messages.last&.timestamp || nil,
+              created_at: local_created_at(messages.last),
               # When this batch was built and scheduled for execution
               scheduled_at: scheduled_at,
               # This needs to be set to a correct value prior to processing starting
               processed_at: nil
             )
+          end
+
+          private
+
+          # Code that aligns the batch creation at into our local time. If time of current machine
+          # and the Kafka cluster drift, this helps not to allow this to leak into the framework.
+          #
+          # @param last_message [::Karafka::Messages::Message, nil] last message from the batch or
+          #   nil if no message
+          # @return [Time] batch creation time. Now if no messages (workless flow) or the last
+          #   message time as long as the message is not from the future
+          # @note Message can be from the future in case consumer machine and Kafka cluster drift
+          #   apart and the machine is behind the cluster.
+          def local_created_at(last_message)
+            now = ::Time.now
+
+            return now unless last_message
+
+            timestamp = last_message.timestamp
+            timestamp > now ? now : timestamp
           end
         end
       end
