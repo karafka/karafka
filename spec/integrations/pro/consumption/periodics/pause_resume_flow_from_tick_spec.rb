fc7650ae262aa330a5be45adb0e73fa31401d85d
Maciej Mensfeld <maciej@mensfeld.pl>
2023-12-11 20:48:02 +0100

    flow control specs

---
+++ spec/integrations/pro/consumption/periodics/pause_resume_flow_from_tick_spec.rb
@@ -0,0 +1,38 @@
+# frozen_string_literal: true
+
+# We should be able to control flow from ticking
+
+setup_karafka do |config|
+  config.max_messages = 1
+end
+
+class Consumer < Karafka::BaseConsumer
+  def consume
+    DT[:consume] << messages.last.offset
+  end
+
+  def tick
+    produce_many(DT.topic, DT.uuids(1))
+
+    if @paused
+      resume
+      @paused = false
+    else
+      @paused = true
+      pause(:consecutive, 1_000_000_000)
+    end
+  end
+end
+
+draw_routes do
+  topic DT.topic do
+    consumer Consumer
+    periodic true
+  end
+end
+
+start_karafka_and_wait_until do
+  DT[:consume].count >= 3
+end
+
+assert_equal [0, 1, 2], DT[:consume][0..2]
---
+++ spec/integrations/pro/consumption/periodics/resume_from_tick_spec.rb
@@ -0,0 +1,36 @@
+# frozen_string_literal: true
+
+# We should be able to resume from ticking
+
+setup_karafka do |config|
+  config.max_messages = 1
+end
+
+class Consumer < Karafka::BaseConsumer
+  def consume
+    DT[:consume] << messages.last.offset
+
+    # This will ensure we won't resume automatically
+    pause(:consecutive, 1_000_000_000)
+  end
+
+  def tick
+    resume
+  end
+end
+
+draw_routes do
+  topic DT.topic do
+    consumer Consumer
+    periodic true
+  end
+end
+
+produce_many(DT.topic, DT.uuids(10))
+
+# If resume from ticking will not work, this will hang
+start_karafka_and_wait_until do
+  DT[:consume].count >= 3
+end
+
+assert_equal [0, 1, 2], DT[:consume][0..2]
