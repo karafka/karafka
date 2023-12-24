bd171f308498ac121c539b2845b689eb3165d280
Maciej Mensfeld <maciej@mensfeld.pl>
2023-12-24 13:21:56 +0100

    alias lrj as non blocking

---
+++ spec/integrations/pro/consumption/strategies/lrj/default/processing_via_non_blocking_spec.rb
@@ -0,0 +1,40 @@
+# frozen_string_literal: true
+
+# When a job is marked as lrj, it should keep running longer than max poll interval and all
+# should be good. It should continue processing after resume and should pick up next messages
+# Marking as LRJ should also work via `non_blocking` alias.
+
+setup_karafka do |config|
+  config.max_messages = 1
+  # We set it here that way not too wait too long on stuff
+  config.kafka[:'max.poll.interval.ms'] = 10_000
+  config.kafka[:'session.timeout.ms'] = 10_000
+end
+
+class Consumer < Karafka::BaseConsumer
+  def consume
+    # Ensure we exceed max poll interval, if that happens and this would not work async we would
+    # be kicked out of the group
+    sleep(15)
+
+    DT[0] << messages.first.raw_payload
+  end
+end
+
+draw_routes do
+  consumer_group DT.consumer_group do
+    topic DT.topic do
+      consumer Consumer
+      non_blocking_job true
+    end
+  end
+end
+
+payloads = DT.uuids(2)
+produce_many(DT.topic, payloads)
+
+start_karafka_and_wait_until do
+  DT[0].size >= 2
+end
+
+assert_equal payloads, DT[0]
---
+++ spec/lib/karafka/pro/routing/features/non_blocking_job/topic_spec.rb
@@ -0,0 +1,46 @@
+# frozen_string_literal: true
+
+RSpec.describe_current do
+  subject(:topic) { build(:routing_topic) }
+
+  describe '#non_blocking_job' do
+    context 'when we use non_blocking_job without any arguments' do
+      it 'expect to initialize with defaults' do
+        expect(topic.long_running_job.active?).to eq(false)
+      end
+    end
+
+    context 'when we use non_blocking_job with active status' do
+      it 'expect to use proper active status' do
+        topic.non_blocking_job(true)
+        expect(topic.long_running_job.active?).to eq(true)
+      end
+    end
+
+    context 'when we use non_blocking_job multiple times with different values' do
+      it 'expect to use proper active status' do
+        topic.non_blocking_job(true)
+        topic.non_blocking_job(false)
+        expect(topic.long_running_job.active?).to eq(true)
+      end
+    end
+  end
+
+  describe '#long_running_job?' do
+    context 'when active' do
+      before { topic.non_blocking_job(true) }
+
+      it { expect(topic.long_running_job?).to eq(true) }
+    end
+
+    context 'when not active' do
+      before { topic.non_blocking_job(false) }
+
+      it { expect(topic.long_running_job?).to eq(false) }
+    end
+  end
+
+  describe '#to_h' do
+    it { expect(topic.to_h[:long_running_job]).to eq(topic.non_blocking_job.to_h) }
+  end
+end
