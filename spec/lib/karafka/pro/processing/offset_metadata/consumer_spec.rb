6aa692b4aec28762e95f99792ae9842873c7bffe
Maciej Mensfeld <maciej@mensfeld.pl>
2023-12-31 11:04:54 +0100

    more specs

--- spec/lib/karafka/pro/processing/offset_metadata/consumer_spec.rb
+++ spec/lib/karafka/pro/processing/offset_metadata/consumer_spec.rb
@@ -1,5 +1,35 @@
 # frozen_string_literal: true
 
 RSpec.describe_current do
-  pending
+  let(:consumer) { Class.new { include Karafka::Pro::Processing::OffsetMetadata::Consumer }.new }
+  let(:topic) { build(:routing_topic) }
+  let(:partition) { 5 }
+  let(:fetcher) { Karafka::Pro::Processing::OffsetMetadata::Fetcher }
+
+  before do
+    allow(consumer).to receive(:topic).and_return(topic)
+    allow(consumer).to receive(:partition).and_return(partition)
+  end
+
+  describe '#offset_metadata' do
+    context 'when assignment is revoked' do
+      before { allow(consumer).to receive(:revoked?).and_return(true) }
+
+      it { expect(consumer.offset_metadata).to eq(false) }
+    end
+
+    context 'when assignment is active' do
+      let(:result) { rand }
+
+      before do
+        allow(consumer).to receive(:revoked?).and_return(false)
+        allow(fetcher).to receive(:find).and_return(result)
+      end
+
+      it 'expect to reach out to fetcher' do
+        expect(consumer.offset_metadata).to eq(result)
+        expect(fetcher).to have_received(:find).with(topic, partition, cache: true)
+      end
+    end
+  end
 end
