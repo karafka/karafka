c70e8bbcd1ee510fbbfe098893f9301310039110
Maciej Mensfeld <maciej@mensfeld.pl>
2024-01-07 19:35:08 +0100

    wip

---
+++ spec/integrations/pro/routing/multiplexing/ignore_whole_group_spec.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+# When we decide to skip subscription group, it should skip all multiplexed sgs
+Consumer = Class.new(Karafka::BaseConsumer)
+
+setup_karafka
+
+draw_routes(create_topics: false) do
+  subscription_group :test do
+    multiplexing(count: 10)
+
+    topic DT.topics[0] do
+      consumer Consumer
+    end
+  end
+end
+
+ARGV[0] = 'server'
+ARGV[1] = '--exclude-subscription-groups'
+ARGV[2] = 'test'
+
+Karafka::Cli.start
+
