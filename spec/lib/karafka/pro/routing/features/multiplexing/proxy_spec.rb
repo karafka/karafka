025b44cd65a2b6a99aa6da306bb09460996bb96e
Maciej Mensfeld <maciej@mensfeld.pl>
2024-01-11 13:08:35 +0100

    prevent from multiplexing root

--- spec/lib/karafka/pro/routing/features/multiplexing/proxy_spec.rb
+++ spec/lib/karafka/pro/routing/features/multiplexing/proxy_spec.rb
@@ -1,3 +1,25 @@
 # frozen_string_literal: true
 
-# Placeholder for Coditsu. Checked in integrations.
+RSpec.describe_current do
+  subject(:builder) { Karafka::Routing::Builder.new }
+
+  let(:topic) { builder.first.topics.first }
+
+  describe '#multiplexing' do
+    before do
+      builder.draw do
+        consumer_group(:a) do
+          subscription_group(:test) do
+            multiplexing(min: 2, max: 3)
+
+            topic(:test) { active(false) }
+          end
+        end
+      end
+    end
+
+    it { expect(topic.subscription_group_details[:multiplexing_min]).to eq(2) }
+    it { expect(topic.subscription_group_details[:multiplexing_max]).to eq(3) }
+    it { expect(topic.subscription_group_details[:name]).to eq('test') }
+  end
+end
