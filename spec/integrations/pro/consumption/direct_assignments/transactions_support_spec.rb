4f6cd4a37d2d59565b413c81ba6c7889b7aefd9f
Maciej Mensfeld <maciej@mensfeld.pl>
2024-03-03 14:12:18 +0100

    transactions support

---
+++ spec/integrations/pro/consumption/direct_assignments/transactions_support_spec.rb
@@ -0,0 +1,37 @@
+# frozen_string_literal: true
+
+# We should be able to assign what we want and mark offsets in a transation
+
+setup_karafka do |config|
+  config.kafka[:'transactional.id'] = SecureRandom.uuid
+  config.max_messages = 10
+end
+
+class Consumer < Karafka::BaseConsumer
+  def consume
+    transaction do
+      messages.each { |message| mark_as_consumed(message) }
+      produce_async(topic: DT.topic, payload: '1')
+    end
+
+    DT[:done] = true
+
+    sleep(2)
+  end
+end
+
+draw_routes do
+  topic DT.topic do
+    consumer Consumer
+    assign(true)
+  end
+end
+
+elements = DT.uuids(100)
+produce_many(DT.topic, elements)
+
+start_karafka_and_wait_until do
+  DT.key?(:done)
+end
+
+assert fetch_first_offset >= 10
