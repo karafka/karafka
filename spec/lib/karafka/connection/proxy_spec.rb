8d2948ff7d9c5de8d304a36e48b22e7fd139c43b
Maciej Mensfeld <maciej@mensfeld.pl>
2024-03-05 11:49:14 +0100

    remarks

--- spec/lib/karafka/connection/proxy_spec.rb
+++ spec/lib/karafka/connection/proxy_spec.rb
@@ -105,4 +105,156 @@ RSpec.describe_current do
       end
     end
   end
+
+  describe '#store_offset' do
+    let(:message) { instance_double('Karafka::Messages::Message') }
+    let(:metadata) { 'metadata' }
+
+    context 'when storing the offset is successful' do
+      before do
+        allow(wrapped_object).to receive(:store_offset).with(message, metadata).and_return(true)
+      end
+
+      it 'returns true' do
+        expect(proxy.store_offset(message, metadata)).to be true
+      end
+    end
+
+    context 'when an assignment_lost error occurs' do
+      # Simulate assignment lost error code
+      let(:assignment_lost_error) { Rdkafka::RdkafkaError.new(-142) }
+
+      before do
+        allow(wrapped_object)
+          .to receive(:store_offset)
+          .with(message, metadata).and_raise(assignment_lost_error)
+      end
+
+      it 'returns false' do
+        expect(proxy.store_offset(message, metadata)).to be false
+      end
+    end
+
+    context 'when a state error occurs' do
+      # Simulate state error code
+      let(:state_error) { Rdkafka::RdkafkaError.new(-172) }
+
+      before do
+        allow(wrapped_object)
+          .to receive(:store_offset)
+          .with(message, metadata)
+          .and_raise(state_error)
+      end
+
+      it 'returns false' do
+        expect(proxy.store_offset(message, metadata)).to be false
+      end
+    end
+
+    context 'when another RdkafkaError occurs' do
+      # Simulate an unspecified error code
+      let(:other_error) { Rdkafka::RdkafkaError.new(-1) }
+
+      before do
+        allow(wrapped_object)
+          .to receive(:store_offset)
+          .with(message, metadata)
+          .and_raise(other_error)
+      end
+
+      it 'raises the error' do
+        expect { proxy.store_offset(message, metadata) }.to raise_error(Rdkafka::RdkafkaError)
+      end
+    end
+  end
+
+  describe '#commit_offsets' do
+    context 'when committing offsets asynchronously' do
+      before do
+        allow(wrapped_object).to receive(:commit).with(nil, true).and_return(true)
+      end
+
+      it 'returns true' do
+        expect(proxy.commit_offsets).to be true
+      end
+    end
+
+    context 'when committing offsets synchronously' do
+      before do
+        allow(wrapped_object).to receive(:commit).with(nil, false).and_return(true)
+      end
+
+      it 'returns true' do
+        expect(proxy.commit_offsets(async: false)).to be true
+      end
+    end
+
+    context 'when an assignment_lost error occurs' do
+      let(:error) { Rdkafka::RdkafkaError.new(-142) }
+
+      before do
+        allow(wrapped_object).to receive(:commit).and_raise(error)
+      end
+
+      it 'returns false' do
+        expect(proxy.commit_offsets).to be false
+      end
+    end
+
+    context 'when an unknown_member_id error occurs' do
+      let(:error) { Rdkafka::RdkafkaError.new(25) }
+
+      before do
+        allow(wrapped_object).to receive(:commit).and_raise(error)
+      end
+
+      it 'returns false' do
+        expect(proxy.commit_offsets).to be false
+      end
+    end
+
+    context 'when a no_offset error occurs' do
+      let(:error) { Rdkafka::RdkafkaError.new(-168) }
+
+      before do
+        allow(wrapped_object).to receive(:commit).and_raise(error)
+      end
+
+      it 'returns true' do
+        expect(proxy.commit_offsets).to be true
+      end
+    end
+
+    context 'when a coordinator_load_in_progress error occurs' do
+      let(:error) { Rdkafka::RdkafkaError.new(14) }
+
+      before do
+        attempts = 0
+        allow(wrapped_object).to receive(:commit) do
+          attempts += 1
+
+          raise error if attempts == 1
+
+          true # Simulates success on retry
+        end
+      end
+
+      it 'retries once and then succeeds' do
+        expect(proxy.commit_offsets).to be true
+        expect(wrapped_object).to have_received(:commit).twice
+      end
+    end
+
+    context 'when another RdkafkaError occurs' do
+      let(:other_error) { Rdkafka::RdkafkaError.new(13) }
+
+      before do
+        allow(wrapped_object).to receive(:commit).and_raise(other_error)
+      end
+
+      it 'raises the error' do
+        expect { proxy.commit_offsets }.to raise_error(Rdkafka::RdkafkaError)
+      end
+    end
+  end
 end
