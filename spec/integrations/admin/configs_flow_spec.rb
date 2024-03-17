d488cc9c412777782acdd2ba4d860d7356653997
Maciej Mensfeld <maciej@mensfeld.pl>
2024-03-17 17:56:48 +0100

    remarks

--- spec/integrations/admin/configs_flow_spec.rb
+++ spec/integrations/admin/configs_flow_spec.rb
@@ -19,11 +19,11 @@ end
 # Lets read broker settings
 broker = Karafka::Admin::Configs::Resource.new(type: :broker, name: '1')
 
-Karafka::Admin::Configs.describe(broker).each do |broker|
-  broker.type
-  broker.name
+Karafka::Admin::Configs.describe(broker).each do |fetched_broker|
+  fetched_broker.type
+  fetched_broker.name
 
-  broker.configs.each do |config|
+  fetched_broker.configs.each do |config|
     config.name
     config.value
     config.read_only?
@@ -36,7 +36,7 @@ end
 topic1 = Karafka::Admin::Configs::Resource.new(type: :topic, name: DT.topics[0])
 topic2 = Karafka::Admin::Configs::Resource.new(type: :topic, name: DT.topics[1])
 
-topic1.set('delete.retention.ms', 86_800_000)
+topic1.set('delete.retention.ms', 86_800_123)
 topic1.set('cleanup.policy', 'compact')
 topic2.delete('delete.retention.ms')
 
@@ -45,3 +45,27 @@ Karafka::Admin::Configs.alter(topic1, topic2)
 # And lets now verify that those settings were indeed changed
 
 rtopic1, rtopic2 = Karafka::Admin::Configs.describe(topic1, topic2)
+
+assert_equal rtopic1.name, DT.topics[0]
+assert_equal rtopic1.type, :topic
+assert_equal rtopic2.name, DT.topics[1]
+assert_equal rtopic2.type, :topic
+
+rtopic1.configs.each do |config|
+  if config.name == 'delete.retention.ms'
+    assert_equal config.name, 'delete.retention.ms'
+    assert_equal config.value, '86800123'
+  end
+
+  if config.name == 'cleanup.policy'
+    assert_equal config.name, 'cleanup.policy'
+    assert_equal config.value, 'compact'
+  end
+end
+
+rtopic2.configs.each do |config|
+  if config.name == 'delete.retention.ms'
+    assert_equal config.name, 'delete.retention.ms'
+    assert_equal config.value, '86400000'
+  end
+end
