ae25c09f27c7d275c32dd19d7a933cc9e0a3d1a8
Maciej Mensfeld <maciej@mensfeld.pl>
2024-03-18 15:04:02 +0100

    remarks

--- spec/lib/karafka/cli/topics/align_spec.rb
+++ spec/lib/karafka/cli/topics/align_spec.rb
@@ -1,5 +1,4 @@
 # frozen_string_literal: true
-# frozen_string_literal: true
 
 # Placeholder. All topics CLI commands are checked in the integration suite.
 # Align is fully tested in the integrations
