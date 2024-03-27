43f850850632fa9592ecff0b142c492816db5450
Maciej Mensfeld <maciej@mensfeld.pl>
2024-03-27 22:13:16 +0100

    testing bump

--- lib/karafka/deserializers/key.rb
+++ lib/karafka/deserializers/key.rb
@@ -6,7 +6,7 @@ module Karafka
     class Key
       # @param metadata [Karafka::Messages::Metadata] metadata object from which we obtain the
       #   `#raw_key`
-      # @return [String] expected message key in a string format
+      # @return [String, nil] expected message key in a string format or nil if no key
       def call(metadata)
         metadata.raw_key
       end
