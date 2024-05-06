1312f849fd8c36ab56769a5e4bd814b905ec80ce
Maciej Mensfeld <maciej@mensfeld.pl>
2024-05-06 15:49:57 +0200

    ensure usable from specs

--- spec/integrations/routing/topic_custom_attributes_spec.rb
+++ spec/integrations/routing/topic_custom_attributes_spec.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 # You should be able to define your own features to assign them to topics
+# You should also be able to use it from the consumer
 
 setup_karafka
 
@@ -25,18 +26,27 @@ end
 
 CustomAttributes.activate
 
-draw_routes(create_topics: false) do
+class Consumer < Karafka::BaseConsumer
+  def consume
+    DT[:state] = topic.custom_attributes
+  end
+end
+
+draw_routes(create_topics: true) do
   defaults do
-    active false
     custom_attributes(mine: 1, yours: 2)
   end
 
-  consumer_group :test do
-    topic 'topic2'
+  consumer_group DT.topics[0] do
+    topic DT.topics[0] do
+      active true
+      consumer Consumer
+    end
   end
 
-  consumer_group :test2 do
-    topic 'topic1' do
+  consumer_group DT.topics[1] do
+    topic DT.topics[1] do
+      active false
       custom_attributes(mine: 3, yours: 4)
     end
   end
@@ -45,9 +55,15 @@ end
 t1 = Karafka::App.consumer_groups[0].topics.first
 t2 = Karafka::App.consumer_groups[1].topics.first
 
-assert_equal 'topic2', t1.name
-assert_equal 'topic1', t2.name
 assert_equal t1.custom_attributes.mine, 1
 assert_equal t1.custom_attributes.yours, 2
 assert_equal t2.custom_attributes.mine, 3
 assert_equal t2.custom_attributes.yours, 4
+
+produce(DT.topics[0], '{}')
+
+start_karafka_and_wait_until do
+  DT.key?(:state)
+end
+
+assert_equal DT[:state], t1.custom_attributes
