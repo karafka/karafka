1bd13fb563b5ed796117937c087a3ad93bf6f6a5
Maciej Mensfeld <maciej@mensfeld.pl>
2024-05-07 14:05:10 +0200

    more seek specs

---
+++ spec/integrations/seek/back_with_reset_and_mark_again_spec.rb
@@ -0,0 +1,48 @@
+# frozen_string_literal: true
+
+# We should be able to seek back and reset the offset + mark as consumed previous offset and it
+# should have been stored
+
+setup_karafka
+
+class Consumer < Karafka::BaseConsumer
+  def initialize
+    super
+  end
+
+  def consume
+    if @seeked && !@marked
+      mark_as_consumed!(messages.first)
+      @marked = true
+      DT[:done] = true
+    end
+
+    return if @seeked
+
+    messages.each do |message|
+      mark_as_consumed!(message)
+
+      if message.offset == 9
+        @seeked = true
+        seek(1, reset_offset: true)
+
+        return
+      end
+    end
+  end
+end
+
+draw_routes do
+  topic DT.topic do
+    consumer Consumer
+    manual_offset_management true
+  end
+end
+
+produce_many(DT.topic, DT.uuids(10))
+
+start_karafka_and_wait_until do
+  DT.key?(:done)
+end
+
+assert_equal 2, fetch_next_offset
---
+++ spec/integrations/seek/back_without_reset_and_mark_again_spec.rb
@@ -0,0 +1,47 @@
+# frozen_string_literal: true
+
+# We should be able to seek back and without reset it should not store the seeked offset
+
+setup_karafka
+
+class Consumer < Karafka::BaseConsumer
+  def initialize
+    super
+  end
+
+  def consume
+    if @seeked && !@marked
+      mark_as_consumed!(messages.first)
+      @marked = true
+      DT[:done] = true
+    end
+
+    return if @seeked
+
+    messages.each do |message|
+      mark_as_consumed!(message)
+
+      if message.offset == 9
+        @seeked = true
+        seek(1, reset_offset: false)
+
+        return
+      end
+    end
+  end
+end
+
+draw_routes do
+  topic DT.topic do
+    consumer Consumer
+    manual_offset_management true
+  end
+end
+
+produce_many(DT.topic, DT.uuids(10))
+
+start_karafka_and_wait_until do
+  DT.key?(:done)
+end
+
+assert_equal 10, fetch_next_offset
