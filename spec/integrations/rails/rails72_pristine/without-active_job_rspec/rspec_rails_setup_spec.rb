754c8e37a54bb5d4b1abe82ff7e7108bad2326dc
Maciej Mensfeld <maciej@mensfeld.pl>
2024-08-14 17:46:02 +0200

    Rails 7.2 integration setup (#2219)

---
+++ spec/integrations/rails/rails72_pristine/without-active_job_rspec/rspec_rails_setup_spec.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+# Karafka should work with Rails 7.2 and rspec/rails when it is required and should not crash
+#
+# @see https://github.com/karafka/karafka/issues/803
+
+# Load all the Railtie stuff like when `rails server`
+ENV['KARAFKA_CLI'] = 'true'
+
+Bundler.require(:default)
+
+ENV['RAILS_ENV'] = 'test'
+
+# This integration spec requires only to load stuff. If nothing crashed, it means all works as
+# expected
+require 'rails'
+require 'active_model/railtie'
+require 'active_record/railtie'
+require 'action_controller'
+require 'action_controller/railtie'
+require 'action_view/railtie'
+require 'rspec/rails'
