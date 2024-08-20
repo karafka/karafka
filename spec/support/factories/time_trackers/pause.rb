56a1e750359d2065d716e7706fc804105fcaaaaa
Maciej Mensfeld <maciej@mensfeld.pl>
2024-08-20 11:05:24 +0200

    remarks

--- spec/support/factories/time_trackers/pause.rb
+++ spec/support/factories/time_trackers/pause.rb
@@ -2,14 +2,20 @@
 
 FactoryBot.define do
   factory :time_trackers_pause, class: 'Karafka::TimeTrackers::Pause' do
-    timeout { 500 }
+    # Broken in factory bot when using Ruby 3.0
+    timeout { 500 } if RUBY_VERSION >= '3.1'
+
     max_timeout { 1_000 }
     exponential_backoff { true }
 
     skip_create
 
     initialize_with do
-      new(timeout: timeout, max_timeout: max_timeout, exponential_backoff: exponential_backoff)
+      if RUBY_VERSION >= '3.1'
+        new(timeout: timeout, max_timeout: max_timeout, exponential_backoff: exponential_backoff)
+      else
+        new(timeout: 500, max_timeout: max_timeout, exponential_backoff: exponential_backoff)
+      end
     end
   end
 end
