e9f463f885c2df8d0c88ef3c9135fa570d9465f2
Maciej Mensfeld <maciej@mensfeld.pl>
2024-08-22 21:43:26 +0200

    skip versioning check (#2236)

--- spec/lib/karafka/pro/recurring_tasks/matcher_spec.rb
+++ spec/lib/karafka/pro/recurring_tasks/matcher_spec.rb
@@ -4,20 +4,18 @@ RSpec.describe_current do
   subject(:matcher) { described_class.new }
 
   let(:task) { Karafka::Pro::RecurringTasks::Task.new(id: 'task 1', cron: '* * * * *') }
-  let(:schedule_version) { '1.0.0' }
   let(:schema_version) { '1.0' }
 
   let(:payload) do
     {
       type: 'command',
       task: { id: task_id },
-      schema_version: schema_version,
-      schedule_version: schedule_version
+      schema_version: schema_version
     }
   end
 
   before do
-    schedule = Karafka::Pro::RecurringTasks::Schedule.new(version: schedule_version)
+    schedule = Karafka::Pro::RecurringTasks::Schedule.new(version: '1.0.1')
     schedule << task
 
     allow(Karafka::Pro::RecurringTasks)
@@ -54,17 +52,6 @@ RSpec.describe_current do
     context 'when schema version in payload does not match' do
       let(:task_id) { task.id }
       let(:schema_version) { '0.9' }
-
-      it 'returns false' do
-        expect(matcher.matches?(task, payload)).to eq(false)
-      end
-    end
-
-    context 'when schedule version in payload does not match current schedule version' do
-      let(:task_id) { task.id }
-      let(:schedule_version) { '2.0.0' }
-
-      before { payload[:schedule_version] = '3.0.0' }
 
       it 'returns false' do
         expect(matcher.matches?(task, payload)).to eq(false)
