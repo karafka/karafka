4da5c324c4ec773e27be21fdc6fa8cb86c316663
Maciej Mensfeld <maciej@mensfeld.pl>
2024-09-10 15:51:36 +0200

    migrate dispatcher aj reference to config importer

--- lib/active_job/queue_adapters/karafka_adapter.rb
+++ lib/active_job/queue_adapters/karafka_adapter.rb
@@ -7,18 +7,22 @@ module ActiveJob
     # Karafka adapter for enqueuing jobs
     # This is here for ease of integration with ActiveJob.
     class KarafkaAdapter
+      include Karafka::Helpers::ConfigImporter.new(
+        dispatcher: %i[internal active_job dispatcher]
+      )
+
       # Enqueues the job using the configured dispatcher
       #
       # @param job [Object] job that should be enqueued
       def enqueue(job)
-        ::Karafka::App.config.internal.active_job.dispatcher.dispatch(job)
+        dispatcher.dispatch(job)
       end
 
       # Enqueues multiple jobs in one go
       # @param jobs [Array<Object>] jobs that we want to enqueue
       # @return [Integer] number of jobs enqueued (required by Rails)
       def enqueue_all(jobs)
-        ::Karafka::App.config.internal.active_job.dispatcher.dispatch_many(jobs)
+        dispatcher.dispatch_many(jobs)
         jobs.size
       end
 
@@ -28,7 +32,7 @@ module ActiveJob
       # @param job [Object] job we want to enqueue
       # @param timestamp [Time] time when job should run
       def enqueue_at(job, timestamp)
-        ::Karafka::App.config.internal.active_job.dispatcher.dispatch_at(job, timestamp)
+        dispatcher.dispatch_at(job, timestamp)
       end
 
       # @return [true] should we by default enqueue after the transaction and not during.
