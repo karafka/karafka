5907d7778688fda3e81d6d4c2510563cad60192b
Maciej Mensfeld <maciej@mensfeld.pl>
2024-10-09 17:00:19 +0200

    introduce strict mode for declarative topics

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,7 @@
 # Karafka Framework Changelog
 
 ## 2.4.13 (Unreleased)
+- [Enhancement] Introduce `config.strict_declarative_topics` that should force declaratives on all non-pattern based topics and DLQ topics
 - [Enhancement] Report ignored repartitioning to lower number of partitions in declarative topics.
 - [Fix] Fix `Karafka::Errors::MissingBootFileError` when debugging in VScode with ruby-lsp.
 - [Fix] Require `karafka-core` `>=` `2.4.4` to prevent dependencies conflicts. 
---
+++ spec/integrations/cli/declaratives/strict_setup_requirement_spec.rb
@@ -0,0 +1,86 @@
+# frozen_string_literal: true
+
+# When we have strict_declarative_topics set to true, we should ensure all non-pattern definitions
+# of topics have their declarative references
+
+setup_karafka do |config|
+  config.strict_declarative_topics = true
+end
+
+ARGV[0] = 'info'
+
+def draw_and_validate(valid:, &block)
+  guarded = false
+
+  begin
+    draw_routes(create_topics: false) do
+      instance_eval(&block)
+    end
+
+    Karafka::Cli.start
+  rescue Karafka::Errors::InvalidConfigurationError
+    guarded = true
+  end
+
+  valid ? assert(!guarded) : assert(guarded)
+
+  Karafka::App.routes.clear
+end
+
+draw_and_validate(valid: false) do
+  topic 'a' do
+    consumer Class.new
+    config(active: false)
+  end
+end
+
+draw_and_validate(valid: true) do
+  topic 'a' do
+    consumer Class.new
+  end
+end
+
+draw_and_validate(valid: false) do
+  topic 'a' do
+    consumer Class.new
+    dead_letter_queue(topic: 'dlq')
+  end
+end
+
+draw_and_validate(valid: true) do
+  topic 'a' do
+    consumer Class.new
+    dead_letter_queue(topic: 'dlq')
+  end
+
+  topic 'dlq' do
+    active(false)
+  end
+end
+
+draw_and_validate(valid: false) do
+  topic 'a' do
+    consumer Class.new
+    dead_letter_queue(topic: 'dlq')
+  end
+
+  topic 'dlq' do
+    active(false)
+    config(active: false)
+  end
+end
+
+# When no strict declaratives
+Karafka::App.config.strict_declarative_topics = false
+
+draw_and_validate(valid: true) do
+  topic 'a' do
+    consumer Class.new
+    dead_letter_queue(topic: 'dlq')
+  end
+
+  topic 'dlq' do
+    active(false)
+    config(active: false)
+  end
+end
