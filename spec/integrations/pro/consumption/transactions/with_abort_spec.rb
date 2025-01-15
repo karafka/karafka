52f64a576f1f116bf8c9c7cf1d29c60cd3b30a71
Maciej Mensfeld <maciej@mensfeld.pl>
2025-01-15 12:22:00 +0100

    more transactional specs (#2411)
    * more transactional specs
    
    * add licenses references

---
+++ spec/integrations/pro/consumption/transactions/with_abort_spec.rb
@@ -0,0 +1,36 @@
+# frozen_string_literal: true
+
+# This code is part of Karafka Pro, a commercial component not licensed under LGPL.
+# See LICENSE for details.
+
+# When a transaction is aborted, it should also fire the consuming event as abort similar to
+# ActiveRecord transaction does not propagate and is handled internally, thus no error.
+
+setup_karafka do |config|
+  config.kafka[:'transactional.id'] = SecureRandom.uuid
+end
+
+Karafka.monitor.subscribe('consumer.consuming.transaction') do
+  DT[:occurred] = true
+end
+
+class Consumer < Karafka::BaseConsumer
+  def consume
+    transaction do
+      mark_as_consumed(messages.last)
+      DT[:done] = true
+
+      raise(WaterDrop::AbortTransaction)
+    end
+  end
+end
+
+draw_routes(Consumer)
+produce_many(DT.topic, DT.uuids(1))
+
+start_karafka_and_wait_until do
+  DT.key?(:done)
+end
+
+assert_equal 0, fetch_next_offset
+assert DT.key?(:occurred)
---
+++ spec/integrations/pro/consumption/transactions/with_lag_less_statistics_spec.rb
@@ -0,0 +1,38 @@
+# frozen_string_literal: true
+
+# This code is part of Karafka Pro, a commercial component not licensed under LGPL.
+# See LICENSE for details.
+
+# When offsets are part of producer transactions, they will no longer appear in the consumer
+# `statistics.emitted` events and should be compensated via Karafka instrumentation.
+
+setup_karafka do |config|
+  config.max_messages = 1
+  config.kafka[:'transactional.id'] = SecureRandom.uuid
+end
+
+Karafka.monitor.subscribe('statistics.emitted') do |event|
+  DT[:statistics] << event[:statistics]
+end
+
+class Consumer < Karafka::BaseConsumer
+  def consume
+    transaction do
+      mark_as_consumed(messages.last)
+    end
+
+    DT[:counts] << true
+  end
+end
+
+draw_routes(Consumer)
+produce_many(DT.topic, DT.uuids(100))
+
+start_karafka_and_wait_until do
+  DT[:statistics].size >= 100 && DT[:counts].size >= 100
+end
+
+p_stats = DT[:statistics].last['topics'][DT.topic]['partitions']['0']
+
+assert_equal(-1, p_stats['consumer_lag'])
+assert_equal(-1, p_stats['consumer_lag_stored'])
