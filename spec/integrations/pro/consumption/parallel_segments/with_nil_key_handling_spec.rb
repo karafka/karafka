a45a1f0e219e3ae8cade679420d60b9d5d3a3745
Maciej Mensfeld <maciej@mensfeld.pl>
2025-02-26 21:44:17 +0100

    remarks

--- spec/integrations/pro/consumption/parallel_segments/with_nil_key_handling_spec.rb
+++ spec/integrations/pro/consumption/parallel_segments/with_nil_key_handling_spec.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
+
 # This code is part of Karafka Pro, a commercial component not licensed under LGPL.
 # See LICENSE for details.
+
 # Messages with nil keys should be handled gracefully and assigned to a consistent group
 
 setup_karafka do |config|
