21a89f0946eaeed49833d3c73f9e9d49807fe6f1
Maciej Mensfeld <maciej@mensfeld.pl>
2025-03-20 09:45:48 +0100

    Add new seek integration specs (#2506)
    * more seek specs
    
    * typo

---
+++ spec/integrations/seek/beyond_high_watermark_offset_spec.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+# Seeking beyond high watermark should trigger reset back to earliest
+
+setup_karafka
+
+class Consumer < Karafka::BaseConsumer
+  def consume
+    DT[:runs] << messages.first.offset
+
+    seek(21)
+  end
+end
+
+draw_routes do
+  topic DT.topic do
+    consumer Consumer
+  end
+end
+
+produce_many(DT.topic, DT.uuids(20))
+
+start_karafka_and_wait_until do
+  DT[:runs].size >= 5
+end
+
+assert_equal DT[:runs].uniq, [0]
