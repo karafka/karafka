7b9b7fa864dcfa7bb9cfd65e17dfeacf7c0bfe23
Maciej Mensfeld <maciej@mensfeld.pl>
2025-04-08 13:14:39 +0200

    support kip-82

---
+++ spec/integrations/consumption/with_array_headers_spec.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+# Karafka should support KIP-82 and should be able to both read and write array headers
+
+setup_karafka(allow_errors: true)
+
+class Consumer < Karafka::BaseConsumer
+  def consume
+    messages.each do |message|
+      DT[:headers] << message.headers
+    end
+  end
+end
+
+draw_routes(Consumer)
+
+HEADERS = { 'a' => 'b', 'c' => %w[d e] }
+
+Karafka.producer.produce_sync(
+  topic: DT.topic,
+  payload: '',
+  headers: HEADERS
+)
+
+start_karafka_and_wait_until do
+  DT.key?(:headers)
+end
+
+assert_equal DT[:headers].first, HEADERS
