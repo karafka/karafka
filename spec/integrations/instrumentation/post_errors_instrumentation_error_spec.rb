001f7cb5b520762e0480c5315c94cfb2fa6718db
Maciej Mensfeld <maciej@mensfeld.pl>
2025-04-28 20:51:38 +0200

    stability remark

--- spec/integrations/instrumentation/post_errors_instrumentation_error_spec.rb
+++ spec/integrations/instrumentation/post_errors_instrumentation_error_spec.rb
@@ -16,8 +16,11 @@ end
 
 draw_routes(Consumer)
 
-Karafka::App.monitor.subscribe('error.occurred') do
-  raise
+# We do not crash on the forceful shutdown error as this layer needs to correctly close librdkafka
+# consumers, otherwise it may segfault
+Karafka::App.monitor.subscribe('error.occurred') do |event|
+  raise if event[:caller].is_a?(Karafka::BaseConsumer)
+  raise if event[:caller].is_a?(Karafka::Processing::Worker)
 end
 
 produce_many(DT.topic, DT.uuids(1))
@@ -31,8 +34,6 @@ begin
   exit 10
 rescue StandardError
   Karafka::Server.stop
-
-  sleep(1)
 
   exit 1
 end
