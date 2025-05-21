76be740d8725f3d745d5425fca3e89851079efe7
Maciej Mensfeld <maciej@mensfeld.pl>
2025-05-21 12:40:02 +0200

    text librdkafka negative lookup (#2616)
    * text librdkafka negative lookup
    
    * doc

---
+++ spec/integrations/pro/consumption/patterns/negative_lookup_spec.rb
@@ -0,0 +1,51 @@
+# frozen_string_literal: true
+
+# This code is part of Karafka Pro, a commercial component not licensed under LGPL.
+# See LICENSE for details.
+
+# We should be able to use posix negative lookup regexps to match all except certain topics
+
+setup_karafka
+
+DT[0] = Set.new
+
+class Consumer < Karafka::BaseConsumer
+  def consume
+    raise "#{topic.name} matched" if topic.name.include?('activities')
+
+    DT[0] << topic
+  end
+end
+
+ENDING = SecureRandom.uuid
+NEGATIVE_MATCHING = <<~PATTERN.gsub(/\s+/, '')
+  ^(
+    [^a]|
+    a[^c]|
+    ac[^t]|
+    act[^i]|
+    acti[^v]|
+    activ[^i]|
+    activi[^t]|
+    activit[^i]|
+    activiti[^e]|
+    activitie[^s]|
+    activities[^.]
+  )
+PATTERN
+
+NEGATIVE_PATTERN = Regexp.new(NEGATIVE_MATCHING + ".*#{Regexp.escape(ENDING)}$")
+
+draw_routes(create_topics: false) do
+  pattern(NEGATIVE_PATTERN) do
+    consumer Consumer
+  end
+end
+
+produce_many("test.#{ENDING}", DT.uuids(1))
+produce_many("activities.#{ENDING}", DT.uuids(1))
+produce_many("active.#{ENDING}", DT.uuids(1))
+
+start_karafka_and_wait_until do
+  DT[0].uniq.size >= 2
+end
