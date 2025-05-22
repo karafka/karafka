6f1749427dcfbaa6b06668bda6cb4ba8cf6724df
Maciej Mensfeld <maciej@mensfeld.pl>
2025-05-22 16:59:41 +0200

    add more time for acl discovery

--- spec/integrations/admin/acl_flow_spec.rb
+++ spec/integrations/admin/acl_flow_spec.rb
@@ -37,7 +37,7 @@ acl2 = Karafka::Admin::Acl.new(
 Karafka::Admin::Acl.create(acl2)
 
 # Give some time to create rule
-sleep(1)
+sleep(5)
 
 assert_equal 1, Karafka::Admin::Acl.describe(acl2).size
 assert Karafka::Admin::Acl.all.map(&:resource_name).include?(uuid2)
