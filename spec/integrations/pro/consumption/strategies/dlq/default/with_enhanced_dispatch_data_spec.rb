7a1a169f111eab934f9e4182e2cfe944a1ead7af
Maciej Mensfeld <maciej@mensfeld.pl>
2025-05-27 16:47:31 +0200

    add license

--- spec/integrations/pro/consumption/strategies/dlq/default/with_enhanced_dispatch_data_spec.rb
+++ spec/integrations/pro/consumption/strategies/dlq/default/with_enhanced_dispatch_data_spec.rb
@@ -1,5 +1,8 @@
 # frozen_string_literal: true
 
+# This code is part of Karafka Pro, a commercial component not licensed under LGPL.
+# See LICENSE for details.
+
 # We should be able to dispatch to DLQ with usage of the enhanced errors tracking details
 
 setup_karafka(allow_errors: %w[consumer.consume.error])
