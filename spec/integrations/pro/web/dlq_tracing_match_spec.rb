971cfa6919f64f923767e8c41828bb01a4053163
Maciej Mensfeld <maciej@mensfeld.pl>
2025-06-04 11:54:40 +0200

    introduce trace spec

---
+++ spec/integrations/pro/web/dlq_tracing_match_spec.rb
@@ -0,0 +1,48 @@
+# frozen_string_literal: true
+
+# Karafka should be able to consume and web tracking should not interfere
+
+setup_karafka(allow_errors: %w[consumer.consume.error])
+setup_web
+
+class Consumer < Karafka::BaseConsumer
+  def consume
+    return if DT[:done].size >= 3
+
+    DT[:done] << true
+
+    raise
+  end
+end
+
+draw_routes do
+  topic DT.topic do
+    consumer Consumer
+
+    dead_letter_queue(
+      topic: DT.topics[1],
+      max_retries: 0,
+      dispatch_method: :produce_sync
+    )
+  end
+
+  topic DT.topics[1] do
+    active(false)
+  end
+end
+
+produce_many(DT.topic, DT.uuids(5))
+
+start_karafka_and_wait_until do
+  DT[:done].size >= 3
+end
+
+dlq_traces = Karafka::Admin
+             .read_topic(DT.topics[1], 0, 5)
+             .map { |message| message.headers['source_trace_id'] }
+
+error_traces = Karafka::Admin
+               .read_topic(Karafka::Web.config.topics.errors.name, 0, 5)
+               .map { |message| message.payload[:details].fetch(:trace_id) }
+
+assert_equal dlq_traces, error_traces
