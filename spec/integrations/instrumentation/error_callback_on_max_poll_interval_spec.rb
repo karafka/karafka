3fde6e1bddda20e2ce2924f97ced2aca229321cc
Maciej Mensfeld <maciej@mensfeld.pl>
2025-06-11 11:10:46 +0200

    max poll error spec for instrumentation

---
+++ spec/integrations/instrumentation/error_callback_on_max_poll_interval_spec.rb
@@ -0,0 +1,32 @@
+# frozen_string_literal: true
+
+# Karafka should publish the max.poll.interval.ms violations into the errors pipeline
+
+setup_karafka(allow_errors: true) do |config|
+  config.kafka[:'max.poll.interval.ms'] = 10_000
+  config.kafka[:'session.timeout.ms'] = 10_000
+end
+
+class Consumer < Karafka::BaseConsumer
+  def consume
+    return if @done
+
+    sleep(15)
+
+    @done = true
+  end
+end
+
+draw_routes(Consumer)
+
+produce(DT.topic, '1')
+
+Karafka::App.monitor.subscribe('error.occurred') do |event|
+  DT[:errors] << event[:error]
+end
+
+start_karafka_and_wait_until do
+  DT.key?(:errors)
+end
+
+assert_equal DT[:errors].last.code, :max_poll_exceeded
