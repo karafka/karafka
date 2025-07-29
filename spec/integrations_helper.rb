a7749e1a37ef04ba8ca8724657542dcb51edf167
Maciej Mensfeld <maciej@mensfeld.pl>
2025-07-29 16:55:43 +0200

    remarks

--- spec/integrations_helper.rb
+++ spec/integrations_helper.rb
@@ -15,6 +15,8 @@ unless ENV.key?('PRISTINE_MODE')
     next if warning.include?('OpenStruct use') && warning.include?('_spec')
     # We redefine whole bunch of stuff to simulate various scenarios
     next if warning.include?('previous definition of') && warning.include?('/integrations/')
+    next if warning.include?('method redefined') && warning.include?('/integrations/')
+
     # Multi-delegator redefining is expected
     next if warning.include?('multi_delegator.rb')
     # We redefine it on purpose
