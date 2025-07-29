22c45fe29efc94f42515a283689ae206aab9c083
Maciej Mensfeld <maciej@mensfeld.pl>
2025-07-29 09:51:04 +0200

    specs

--- spec/lib/karafka/instrumentation/assignments_tracker_spec.rb
+++ spec/lib/karafka/instrumentation/assignments_tracker_spec.rb
@@ -71,4 +71,72 @@ RSpec.describe_current do
 
     it { expect(tracker.current.values.first).to eq([2]) }
   end
+
+  describe '#inspect' do
+    let(:subscription_group) { build(:routing_subscription_group) }
+    let(:topic) { subscription_group.topics.first }
+
+    let(:assign_event) do
+      tpl = Rdkafka::Consumer::TopicPartitionList.new
+      tpl.add_topic(topic.name, [0, 1, 2])
+      { subscription_group: subscription_group, tpl: tpl }
+    end
+
+    context 'when tracker has no assignments' do
+      it 'expect to show empty assignments' do
+        expect(tracker.inspect).to include('assignments={}')
+      end
+    end
+
+    context 'when tracker has assignments' do
+      before { tracker.on_rebalance_partitions_assigned(assign_event) }
+
+      it 'expect to show current assignments with topic names' do
+        result = tracker.inspect
+
+        expect(result).to include('assignments=')
+        expect(result).to include(topic.name)
+        expect(result).to include('[0, 1, 2]')
+      end
+    end
+
+    context 'when mutex is locked' do
+      it 'expect to show busy status without blocking' do
+        tracker.instance_variable_get(:@mutex).lock
+
+        begin
+          expect(tracker.inspect).to include('busy')
+        ensure
+          tracker.instance_variable_get(:@mutex).unlock
+        end
+      end
+    end
+
+    context 'with concurrent access' do
+      it 'expect to handle concurrent inspect and assignment changes safely' do
+        errors = []
+        inspecting = true
+
+        modifier = Thread.new do
+          while inspecting
+            tracker.on_rebalance_partitions_assigned(assign_event)
+            tracker.clear
+          end
+        rescue StandardError => e
+          errors << e
+        end
+
+        inspector = Thread.new do
+          1_000_000.times { tracker.inspect }
+          inspecting = false
+        rescue StandardError => e
+          errors << e
+        end
+
+        [modifier, inspector].each(&:join)
+
+        expect(errors).to be_empty
+      end
+    end
+  end
 end
