be21cd91f0c57ecf69130a4a45c9c4d34b618488
Maciej Mensfeld <maciej@mensfeld.pl>
2025-09-25 10:54:40 +0200

    align spec

--- spec/integrations/consumption/with_producer_disconnect_error_spec.rb
+++ spec/integrations/consumption/with_producer_disconnect_error_spec.rb
@@ -1,16 +1,12 @@
 # frozen_string_literal: true
 
-# When Karafka producer (WaterDrop) disconnects it should instrument and reconnect.
+# When Karafka producer (WaterDrop) disconnects it should reconnect automatically.
 # @note Here we force the disconnect by setting the `connections.max.idle.ms` to a really low
-#   value. Usually you have it higher and it may be, that it is Kafka that is disconnecting.
-#   In such cases you can ignore those errors as the reconnect will happen.
+#   value. With librdkafka updates, planned disconnections no longer emit "all brokers down"
+#   errors, so we test that the producer continues working after forced disconnection.
 
 setup_karafka(allow_errors: true) do |config|
   config.kafka.merge!('connections.max.idle.ms': 1_000)
-end
-
-Karafka.producer.monitor.subscribe('error.occurred') do |event|
-  DT[:errors] << event[:error]
 end
 
 class Consumer < Karafka::BaseConsumer
@@ -27,11 +23,16 @@ produce_many(DT.topic, ['1'])
 
 sleep(5)
 
-produce_many(DT.topic, ['1'])
+produce_many(DT.topic, ['2'])
+
+# Wait longer to allow potential disconnect/reconnect to happen again
+sleep(2)
+
+# Produce another message after potential disconnect to verify producer still works
+produce_many(DT.topic, ['3'])
 
 start_karafka_and_wait_until do
-  DT[0].size >= 2 && DT.key?(:errors)
+  DT[0].size >= 3
 end
 
-assert_equal 2, DT[0].size
-assert DT.key?(:errors)
+assert_equal 3, DT[0].size
