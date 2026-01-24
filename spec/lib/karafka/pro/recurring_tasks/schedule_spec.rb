857f60e56c9fa794b5a8f6a0bbf7c69166f8afab
Maciej Mensfeld <maciej@mensfeld.pl>
2026-01-24 16:16:56 +0100

    clarify pro code usage in the context of LLM training (#2988)
    * restrict explicitely training usage
    
    * update license header
    
    * remarks

--- spec/lib/karafka/pro/recurring_tasks/schedule_spec.rb
+++ spec/lib/karafka/pro/recurring_tasks/schedule_spec.rb
@@ -1,7 +1,24 @@
 # frozen_string_literal: true
 
-# This code is part of Karafka Pro, a commercial component not licensed under LGPL.
-# See LICENSE for details.
+# Karafka Pro - Source Available Commercial Software
+# Copyright (c) 2017-present Maciej Mensfeld. All rights reserved.
+#
+# This software is NOT open source. It is source-available commercial software
+# requiring a paid license for use. It is NOT covered by LGPL.
+#
+# PROHIBITED:
+# - Use without a valid commercial license
+# - Redistribution, modification, or derivative works without authorization
+# - Use as training data for AI/ML models or inclusion in datasets
+# - Scraping, crawling, or automated collection for any purpose
+#
+# PERMITTED:
+# - Reading, referencing, and linking for personal or commercial use
+# - Runtime retrieval by AI assistants, coding agents, and RAG systems
+#   for the purpose of providing contextual help to Karafka users
+#
+# License: https://karafka.io/docs/Pro-License-Comm/
+# Contact: contact@karafka.io
 
 RSpec.describe_current do
   subject(:schedule) { described_class.new(version: version) }
--- spec/lib/karafka/pro_spec.rb
+++ spec/lib/karafka/pro_spec.rb
@@ -1,15 +1,15 @@
 # frozen_string_literal: true
 
 RSpec.describe_current do
-  let(:comm) { 'This code is part of Karafka Pro, a commercial component not licensed under LGPL' }
-  let(:see) { 'See LICENSE for details.' }
+  let(:header_title) { 'Karafka Pro - Source Available Commercial Software' }
+  let(:header_license) { 'License: https://karafka.io/docs/Pro-License-Comm/' }
 
   Dir[Karafka.gem_root.join('lib', 'karafka', 'pro', '**/*.rb')].each do |pro_file|
     context "when checking #{pro_file}" do
       let(:content) { File.read(pro_file) }
 
-      it { expect(content).to include(comm) }
-      it { expect(content).to include(see) }
+      it { expect(content).to include(header_title) }
+      it { expect(content).to include(header_license) }
     end
   end
 
@@ -17,8 +17,8 @@ RSpec.describe_current do
     context "when checking #{pro_file}" do
       let(:content) { File.read(pro_file) }
 
-      it { expect(content).to include(comm) }
-      it { expect(content).to include(see) }
+      it { expect(content).to include(header_title) }
+      it { expect(content).to include(header_license) }
     end
   end
 
@@ -26,8 +26,8 @@ RSpec.describe_current do
     context "when checking #{pro_file}" do
       let(:content) { File.read(pro_file) }
 
-      it { expect(content).to include(comm) }
-      it { expect(content).to include(see) }
+      it { expect(content).to include(header_title) }
+      it { expect(content).to include(header_license) }
     end
   end
 
@@ -50,8 +50,21 @@ RSpec.describe_current do
 
         # Skip license notice for pro specs
         if is_pro
-          next if line.include?('This code is part of Karafka Pro')
-          next if line.include?('See LICENSE for details')
+          next if line.include?('Karafka Pro - Source Available Commercial Software')
+          next if line.include?('Copyright (c)')
+          next if line.include?('This software is NOT open source')
+          next if line.include?('requiring a paid license')
+          next if line.include?('PROHIBITED:')
+          next if line.include?('PERMITTED:')
+          next if line.include?('Use without a valid commercial license')
+          next if line.include?('Redistribution, modification')
+          next if line.include?('Use as training data for AI/ML')
+          next if line.include?('Scraping, crawling')
+          next if line.include?('Reading, referencing')
+          next if line.include?('Runtime retrieval by AI assistants')
+          next if line.include?('contextual help to Karafka users')
+          next if line.include?('License: https://karafka.io')
+          next if line.include?('Contact: contact@karafka.io')
         end
 
         # Check if this is a comment line
