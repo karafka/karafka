50bf50ff1163c5d31006acf5de48425489a37d90
Maciej Mensfeld <maciej@mensfeld.pl>
2026-02-03 11:13:31 +0100

    Add version method to License test mocks for compatibility with license_fully_defined? check
    The optimized license loading now checks for both token and version methods on the License module. Updated all test mocks to include the version method to match the expected interface.

--- spec/integrations_helper.rb
+++ spec/integrations_helper.rb
@@ -231,6 +231,10 @@ def become_pro!
     def self.token
       ENV.fetch("KARAFKA_PRO_LICENSE_TOKEN")
     end
+
+    def self.version
+      "1.0.0"
+    end
   end
 
   Karafka.const_set(:License, mod) unless Karafka.const_defined?(:License)
