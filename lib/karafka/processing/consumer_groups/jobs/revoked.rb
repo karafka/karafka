acf785371465ecb1db64ab02324f438f7e8b6227
Maciej Mensfeld <maciej@mensfeld.pl>
2026-04-17 09:29:43 +0200

    Fix typos in Revoked job docstrings (loose→lose, suppose→supposed)

--- lib/karafka/processing/consumer_groups/jobs/revoked.rb
+++ lib/karafka/processing/consumer_groups/jobs/revoked.rb
@@ -7,11 +7,11 @@ module Karafka
     module ConsumerGroups
       # Consumer-group-specific job types
       module Jobs
-        # Job that runs the revoked operation when we loose a partition on a consumer that lost it.
+        # Job that runs the revoked operation when we lose a partition on a consumer that lost it.
         class Revoked < Processing::Jobs::Base
           self.action = :revoked
 
-          # @param executor [Karafka::Processing::ConsumerGroups::Executor] executor that is suppose to run the job
+          # @param executor [Karafka::Processing::ConsumerGroups::Executor] executor that is supposed to run the job
           # @return [Revoked]
           def initialize(executor)
             @executor = executor
